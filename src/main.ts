3afe73b1adfc89aeb9c07be45dead7493bef63fd
Don Maxito <maxharz@gmail.com>
2018-08-17 00:49:08 +0200

    notify client when window is closed

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,7 @@ import router from './router';
 import store from './store';
 import AsyncComputed from 'vue-async-computed';
 import RpcApi from '@/lib/RpcApi';
+import { ResponseStatus } from '@nimiq/rpc';
 
 Vue.config.productionTip = false;
 Vue.use(AsyncComputed);
@@ -16,3 +17,7 @@ new Vue({
 
 // Start RPC Api
 new RpcApi(store, router).start();
+
+window.addEventListener('beforeunload', () => {
+    store!.state!.rpcState!.reply(ResponseStatus.ERROR, new Error('Window was closed'));
+});
