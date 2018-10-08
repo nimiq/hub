13c05d3a6b5695b19a3a5756d234ad8e83c68d3e
sisou <hello@soerenschwert.de>
2018-10-08 09:44:02 +0200

    Start converting px to rem

--- src/App.vue
+++ src/App.vue
@@ -63,22 +63,27 @@ export default class App extends Vue {
         background: linear-gradient(55deg, #2462dc, #a83df6);
         background-size: cover;
         background-attachment: fixed;
+        font-size: 8px;
+    }
+
+    body {
+        font-size: 2.25rem;
     }
 
     .logo-container {
         width: 100%;
-        padding: 32px;
+        padding: 4rem;
         box-sizing: border-box;
     }
 
     .logo {
-        height: 29px;
+        height: 3.625rem;
         box-sizing: border-box;
-        padding-left: 48px;
-        font-size: 17px;
+        padding-left: 6rem;
+        font-size: 2.125rem;
         font-weight: 600;
         text-transform: uppercase;
-        letter-spacing: 1.3px;
+        letter-spacing: 0.077em;
         display: inline-flex;
         align-items: center;
         color: white;
@@ -89,7 +94,7 @@ export default class App extends Vue {
         outline: none !important;
         background-image: url('data:image/svg+xml,<svg xmlns="http://www.w3.org/2000/svg" width="499" height="440"><path fill="%23FFC107" fill-rule="evenodd" d="M389 21c-6-12-23-21-36-21H145c-13 0-29 9-36 21L5 199c-6 11-6 30 0 41l104 178c7 12 23 21 36 21h208c14 0 30-9 36-21l104-178c7-11 7-30 0-41L389 21zM273 347v42h-39v-40c-24-3-52-13-70-30l25-39c21 15 38 23 57 23 23 0 33-9 33-28 0-40-106-39-106-111 0-38 23-65 61-73V51h39v40c25 3 44 16 59 32l-29 33c-15-13-27-20-44-20-19 0-29 8-29 26 0 37 105 34 105 110 0 37-21 67-62 75z"/></svg>');
         background-repeat: no-repeat;
-        background-size: 33px 29px;
+        background-size: 4.125rem 3.625rem;
     }
 
     #app {
@@ -97,6 +102,7 @@ export default class App extends Vue {
         display: flex;
         flex-direction: column;
         align-items: center;
+        padding: 0 1rem;
     }
 
     .loading {
@@ -113,19 +119,19 @@ export default class App extends Vue {
         background-size: 100%;
         z-index: 1;
         display: block;
-        height: 80px;
-        width: 80px;
+        height: 10rem;
+        width: 10rem;
     }
 
     .loading-animation + h2 {
-        margin-top: 32px;
-        padding-bottom: 64px;
+        margin-top: 4rem;
+        padding-bottom: 8rem;
         color: white;
         text-transform: uppercase;
-        font-size: 14px;
+        font-size: 1.75rem;
         font-weight: 600;
         line-height: 0.86;
-        letter-spacing: 2px;
+        letter-spacing: 0.143em;
     }
 
     /****************
@@ -140,13 +146,13 @@ export default class App extends Vue {
 
     button,
     [button] {
-        font-size: 16px;
+        font-size: 2rem;
         font-weight: 600;
         text-transform: uppercase;
-        letter-spacing: 1.5px;
+        letter-spacing: 0.094em;
         width: 100%;
-        border-radius: 32px;
-        padding: 22px 48px;
+        border-radius: 4rem;
+        padding: 2.75rem 6rem;
         vertical-align: middle;
         display: table-cell;
         text-align: center;
@@ -154,7 +160,7 @@ export default class App extends Vue {
         color: var(--main-button-color);
         cursor: pointer;
         user-select: none;
-        box-shadow: 0 10px 14px 0 rgba(0, 0, 0, 0.15);
+        box-shadow: 0 1.25rem 1.75rem 0 rgba(0, 0, 0, 0.15);
         border: none;
         outline: none;
         line-height: 1.25;
@@ -164,10 +170,7 @@ export default class App extends Vue {
 
     button[disabled],
     [button][disabled] {
-        background: transparent;
-        box-shadow: none;
-        color: white;
-        border: .125rem solid rgba(255, 255, 255, 0.31);
+        opacity: 0.6;
         pointer-events: none;
     }
 </style>
