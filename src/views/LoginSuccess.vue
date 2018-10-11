38b1b8a4d1f62acd9710117e981e7b007a59971e
sisou <hello@soerenschwert.de>
2018-10-11 17:08:20 +0200

    Update remaining success views

--- src/views/LoginSuccess.vue
+++ src/views/LoginSuccess.vue
@@ -75,31 +75,31 @@ export default class LoginSuccess extends Vue {
         justify-content: flex-start;
         align-items: center;
         flex-grow: 1;
-        margin: 8px;
+        margin: 1rem;
         background: #24bdb6;
         color: white;
-        padding: 0 54px;
+        padding: 0 6.75rem;
     }
 
     .icon-checkmark-circle {
-        width: 99px;
-        height: 99px;
-        margin-top: 96px;
+        width: 12.375rem;
+        height: 12.375rem;
+        margin-top: 12rem;
         background-image: url('data:image/svg+xml,<svg version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 100 100" style="enable-background:new 0 0 100 100;" xml:space="preserve"><path d="M50,95C25.19,95,5,74.81,5,50S25.19,5,50,5s45,20.19,45,45S74.81,95,50,95z M50,0C22.43,0,0,22.43,0,50 s22.43,50,50,50s50-22.43,50-50S77.57,0,50,0z M81.41,29.11c-1.01-0.95-2.59-0.9-3.53,0.1L41.2,68.12L19.57,50.56 c-1.07-0.87-2.65-0.71-3.52,0.36c-0.87,1.07-0.71,2.65,0.36,3.52l23.44,19.02c0.46,0.38,1.02,0.56,1.58,0.56 c0.67,0,1.33-0.26,1.82-0.78l38.27-40.59C82.47,31.64,82.42,30.06,81.41,29.11z" fill="%23fff"/></svg>');
         background-repeat: no-repeat;
         background-size: 100%;
     }
 
     h1 {
-        font-size: 30px;
+        font-size: 3.75rem;
         font-weight: 300;
         line-height: 1.3;
-        letter-spacing: 0.5px;
+        letter-spacing: 0.017em;
         text-align: center;
     }
 
     button {
         color: #2a60dd;
-        margin: 24px 0;
+        margin: 3rem 0;
     }
 </style>
