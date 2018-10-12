1d2bb792e9ea1622465c9ccbbf4e3780f55c8777
Sebastian <sebastian@nimiq.com>
2018-10-12 10:13:44 +0200

    removed keyLabel from AccountsManager requests

--- src/views/ExportFile.vue
+++ src/views/ExportFile.vue
@@ -15,6 +15,7 @@ import {State} from 'vuex-class';
 import RpcApi from '../lib/RpcApi';
 import {ExportFileRequest, ExportFileResult as KExportFileResult} from '@nimiq/keyguard-client';
 import {State as RpcState, ResponseStatus} from '@nimiq/rpc';
+import { KeyStore } from '@/lib/KeyStore';
 import staticStore, {Static} from '../lib/StaticStore';
 
 @Component({components: {SmallPage}})
@@ -23,7 +24,7 @@ export default class ExportFile extends Vue {
     @Static private request!: ParsedExportFileRequest;
     @State private keyguardResult!: KExportFileResult | Error | null;
 
-    public created() {
+    public async created() {
         if (this.keyguardResult instanceof Error) {
             this.rpcState.reply(ResponseStatus.ERROR, this.keyguardResult);
         } else if (this.keyguardResult) {
@@ -33,10 +34,13 @@ export default class ExportFile extends Vue {
             this.rpcState.reply(ResponseStatus.OK, result);
         }
 
+        const key = await KeyStore.Instance.get(this.request.keyId);
+        if (!key) throw new Error('KeyId not found');
+
         const request: ExportFileRequest = {
             appName: this.request.appName,
             keyId: this.request.keyId,
-            keyLabel: this.request.keyLabel,
+            keyLabel: key.label,
         };
 
         const client = RpcApi.createKeyguardClient(this.$store, staticStore);
