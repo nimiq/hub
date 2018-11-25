6368ecececa535d17f48365562e49da5999dfa8a
sisou <hello@soerenschwert.de>
2018-11-25 16:12:02 -0600

    Update client build

--- client/dist/AccountsClient.umd.js
+++ client/dist/AccountsClient.umd.js
@@ -120,6 +120,7 @@
     (function (RequestType) {
         RequestType["LIST"] = "list";
         RequestType["CHECKOUT"] = "checkout";
+        RequestType["SIGN_MESSAGE"] = "sign-message";
         RequestType["SIGN_TRANSACTION"] = "sign-transaction";
         RequestType["SIGNUP"] = "signup";
         RequestType["LOGIN"] = "login";
@@ -129,6 +130,7 @@
         RequestType["CHANGE_PASSPHRASE"] = "change-passphrase";
         RequestType["LOGOUT"] = "logout";
         RequestType["ADD_ACCOUNT"] = "add-account";
+        RequestType["RENAME"] = "rename";
     })(RequestType || (RequestType = {}));
 
     class AccountsClient {
@@ -176,6 +178,12 @@
         }
         addAccount(request, requestBehavior = this._defaultBehavior) {
             return this._request(requestBehavior, RequestType.ADD_ACCOUNT, [request]);
+        }
+        rename(request, requestBehavior = this._defaultBehavior) {
+            return this._request(requestBehavior, RequestType.RENAME, [request]);
+        }
+        signMessage(request, requestBehavior = this._defaultBehavior) {
+            return this._request(requestBehavior, RequestType.SIGN_MESSAGE, [request]);
         }
         /**
          * Only accessible in iframe from Nimiq domains.
