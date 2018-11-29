fb78679827211ac02f470f29c543dbd7c263114d
Sven <sven@nimiq.com>
2018-11-29 10:24:34 -0600

    v1 of CookieJar + tests

--- src/lib/CookieJar.ts
+++ src/lib/CookieJar.ts
@@ -3,97 +3,226 @@ import { AccountInfoEntry } from '@/lib/AccountInfo';
 import { ContractInfo } from '/ContractInfo';
 
 enum Separator {
-    ACCOUNT_ELEMENT = '︙',
-    ACCOUNT = '⸾',
-    WALLET_ELEMENT = '⸽',
-    WALLET = '⸻',
+    ACCOUNT_ELEMENT = '_',
+    ACCOUNT = "\\",
+    WALLET_ELEMENT = '~',
+    WALLET = "^",
 }
 
 const BASE64 =
-//   0       8       16      24      32      40      48      56     63
+    //   0       8       16      24      32      40      48      56     63
     "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz+-";
+const BASE64_LOOKUP = new Map<string, number>();
+BASE64.split('').forEach((character, value) => BASE64_LOOKUP.set(character, value));
 
 export class CookieJar {
 
-    private static sanitizeString(string: string): string {
+    public static Separator = Separator;
+
+    static sanitizeString(string: string): string {
         for (const separator in Separator) {
             string = string.replace(separator, '');
         }
         return string;
     }
 
-    private static base64Encode(data: Uint8Array): string{
-        if(data.length % 3 != 0) throw new Error('Can only compress multiples of 3 (Base 64 > 6 bits > chunks of 24bit = 3 bytes)');
+    static base64Encode(data: Uint8Array, pad = false): string {
+        if (!pad && data.length % 3 != 0) throw new Error(`Data is ${data.length} bytes. Can only compress multiples of 3 (Base 64 > 6 bits > chunks of 24bit = 3 bytes)`);
         const chunks: number[] = [];
         for (let index = 0; index < data.length; index += 3) {
             // extract 4x 6 bit chunks from 3 bytes of data
             chunks.push(...
                 [(data[index] & 0xfc) >> 2,
-                ((data[index] & 0x03) << 4) | ((data[index + 1] & 0x0f) >> 4),
-                ((data[index + 1] & 0x0f) << 4) | ((data[index + 2] & 0xc0) >> 6),
+                ((data[index] & 0x03) << 4) | ((data[index + 1] & 0xf0) >> 4),
+                ((data[index + 1] & 0x0f) << 2) | ((data[index + 2] & 0xc0) >> 6),
                 (data[index + 2] & 0x3f)]);
         }
-        return chunks.map(bitChunk => BASE64[bitChunk]).join();
+        return chunks.map(bitChunk => BASE64[bitChunk]).join('');
     }
 
-    private static compressNumber(number: number): string{
-        throw "not implemented"
+    static base64Decode(data: string, pad = 0): Uint8Array {
+        if (data.length % 4 != 0) throw new Error(`Data is ${data.length} bytes. Can only compress multiples of 4 (Base 64 > 6 bits > chunks of 24bit = 4 characters for 3 bytes)`);
+        const bytes: number[] = [];
+        const values = <number[]>data.split('').map(character => BASE64_LOOKUP.get(character));
+        for (let index = 0; index < values.length; index += 4) {
+            // extract 4x 6 bit chunks from 3 bytes of data
+            // base64       AAAAAA BBBBBB CCCCCC DDDDDD
+            // bits of byte 111111 112222 222233 333333
+            bytes.push(...
+                [(values[index] << 2) | ((values[index + 1] & 0x30) >> 4),
+                ((values[index + 1] & 0x0f) << 4) | ((values[index + 2] & 0x3c) >> 2),
+                ((values[index + 2] & 0x03) << 6) | values[index + 3]]);
+        }
+        return new Uint8Array(pad ? bytes.slice(0, pad) : bytes);
     }
 
-    private static compressPath(path: string): string{
+    static encodeNumber(number: number): string {
+        // TODO could be base 64, not much saving though
+        return number.toString(36);
+    }
+
+    static decodeNumber(number: string): number {
+        // try { return parseInt(number, 36) } catch (e) { return 0 };
+        return parseInt(number, 36);
+    }
+
+    static encodeAddress(address: Uint8Array): string {
+        return this.base64Encode(address, true);
+    }
+
+    static decodeAddress(address: string): Uint8Array {
+        return this.base64Decode(address, 20);
+    }
+
+    static encodePath(path: string): string {
         // "m/44'/242'/0'/0'" > var length, restricted character set
         // TODO m/ == constant?
         // > path.split("'/") > 4xFF
         return path;
     }
 
-    private static compressId(hex: string): string{
+    static decodePath(path: string): string {
+        // TODO
+        return path;
+    }
+
+    static encodeId(hex: string): string {
         // example "1ee3d926a49c"
-        if (hex.length != 6) throw new Error('not a valid ID');
+        if (hex.length != 12) throw new Error(`not a valid ID ${hex}`);
         const bytes = hex.split(/(.{2})/).filter(e => e != '').map(e => parseInt(e, 16));
         return this.base64Encode(Uint8Array.from(bytes));
     }
+    static decodeId(code: string): string {
+        const hex = Array.from(this.base64Decode(code)).map(n => n.toString(16)).join('');
+        if (hex.length != 12) throw new Error(`not a valid ID ${hex}`);
+        return hex;
+    }
 
-    private static compressAccount(account: AccountInfoEntry): string {
+    static encodeAccount(account: AccountInfoEntry): string {
         const data = [
-            this.base64Encode(account.address),
-            this.compressPath(account.path),
+            [
+                this.encodeAddress(account.address),
+                this.encodePath(account.path),
+            ].join(''),
             this.sanitizeString(account.label),
         ]
         if (account.balance) {
-            data.push(this.compressNumber(account.balance));
+            data.push(this.encodeNumber(account.balance));
         }
         return data.join(Separator.ACCOUNT_ELEMENT);
     }
 
-    private static compressAccounts(accountsMap: Map</*address*/ string, AccountInfoEntry>): string {
+    static decodeAccount(accountData: string): AccountInfoEntry {
+        const elements = accountData.split(Separator.ACCOUNT_ELEMENT);
+        return {
+            address: this.decodeAddress(elements[0].substr(0, 28)),
+            path: this.decodePath(elements[0].substr(28)),
+            label: elements[1],
+            balance: (elements.length > 2) ? this.decodeNumber(elements[2]) : undefined,
+        };
+    }
+
+    static compressAccounts(accountsMap: Map</*address*/ string, AccountInfoEntry>): string {
         return Array.from(accountsMap.values())
-            .map(account => this.compressAccount(account))
+            .map(account => this.encodeAccount(account))
             .join(Separator.ACCOUNT);
     }
 
-    private static compressContracts(contracts: ContractInfo[]): string {
-        throw "not implemented";
+    static decodeAccounts(accounts: string): Map</*address*/ string, AccountInfoEntry> {
+        const accountsMap = new Map</*address*/ string, AccountInfoEntry>();
+        accounts
+            .split(Separator.ACCOUNT)
+            .forEach(encoded => {
+                const account = this.decodeAccount(encoded);
+                const address = new Nimiq.Address(account.address).toUserFriendlyAddress();
+                accountsMap.set(address, account);
+            });
+
+        return accountsMap;
     }
 
-    private static compressType(type: WalletType): string {
-        // TODO compress!
-        return WalletType[type].toString();
+    static encodeContractType(type: ContractType): string {
+        return type.valueOf().toString(36);
     }
 
-    private static compressWallet(wallet: WalletInfoEntry): string {
-        const data = [
-            `${this.compressId(wallet.id)}${this.compressType(wallet.type)}${this.sanitizeString(wallet.label)}`,
+    static decodeContractType(type: string): ContractType {
+        return <ContractType>parseInt(type, 36);
+    }
+
+    static encodeContract(contract: ContractInfo): string {
+        return [
+            `${this.encodeAddress(contract.address)}${this.encodeContractType(contract.type)}${this.sanitizeString(contract.label)}`,
+            this.encodePath(contract.ownerPath),
+        ].join(Separator.ACCOUNT_ELEMENT);
+    }
+
+    static decodeContract(contract: string): ContractInfo {
+        const elements = contract.split(Separator.ACCOUNT_ELEMENT);
+        return {
+            address: this.decodeAddress(elements[0].substr(0, 28)),
+            type: this.decodeContractType(elements[0].substr(28, 1)),
+            label: this.sanitizeString(elements[0].substr(29)),
+            ownerPath: this.decodePath(elements[1]),
+        }
+    }
+
+    static encodeContracts(contracts: ContractInfo[]): string {
+        return contracts
+            .map(contract => this.encodeContract(contract))
+            .join(Separator.ACCOUNT);
+    }
+
+    static decodeContracts(contracts: string): ContractInfo[] {
+        return contracts
+            .split(Separator.ACCOUNT)
+            .filter(s => s.trim() != '')
+            .map(contract => this.decodeContract(contract));
+    }
+
+    static encodeType(type: WalletType): string {
+        return type.valueOf().toString(36);
+    }
+
+    static decodeType(type: string): WalletType {
+        return <WalletType>parseInt(type, 36);
+    }
+
+    static encodeWallet(wallet: WalletInfoEntry): string {
+        return [
+            [
+                this.encodeId(wallet.id),         // 8 characters
+                this.encodeType(wallet.type),      // 1
+                this.sanitizeString(wallet.label), // variable
+            ].join(''),
             this.compressAccounts(wallet.accounts),
-            this.compressContracts(wallet.contracts),
-        ];
-        return data.join(Separator.WALLET_ELEMENT);
+            this.encodeContracts(wallet.contracts),
+        ].join(Separator.WALLET_ELEMENT);
+    }
+
+    static decodeWallet(wallet: string): WalletInfoEntry {
+        const elements = wallet.split(Separator.WALLET_ELEMENT)
+        return {
+            id: this.decodeId(elements[0].substr(0, 8)),
+            type: this.decodeType(elements[0].substr(8, 1)),
+            label: elements[0].substr(9),
+            accounts: this.decodeAccounts(elements[1]),
+            contracts: this.decodeContracts(elements[2]),
+        }
+    }
+
+    public static encodeWallets(wallets: WalletInfoEntry[]): string {
+        return wallets
+            .map(wallet => this.encodeWallet(wallet))
+            .join(Separator.WALLET);
+    }
+
+    public static decodeWallets(wallets: string): WalletInfoEntry[] {
+        return wallets.split(Separator.WALLET)
+            .map(wallet => this.decodeWallet(wallet))
     }
 
     public static fill(wallets: WalletInfoEntry[]) {
-        return wallets
-            .map(wallet => this.compressWallet(wallet))
-            .join(Separator.WALLET);
+        document.cookie = this.encodeWallets(wallets);
     }
 
 }
---
+++ tests/unit/CookieJar.spec.ts
@@ -0,0 +1,129 @@
+import { WalletInfo, WalletType } from '@/lib/WalletInfo';
+import { AccountInfo } from '@/lib/AccountInfo';
+import { ContractType } from '@/lib/ContractInfo';
+import { CookieJar } from '@/lib/CookieJar';
+
+const Nimiq = require('@nimiq/core'); // tslint:disable-line:no-var-requires variable-name
+// @ts-ignore
+global.Nimiq = Nimiq;
+
+const DUMMY_ADDRESS_HR = 'NQ86 6D3H 6MVD 2JV4 N77V FNA5 M9BL 2QSP 1P64';
+const BURN_ADDRESS_HR  = 'NQ07 0000 0000 0000 0000 0000 0000 0000 0000';
+const DUMMY_ADDRESS: Nimiq.Address = Nimiq.Address.fromUserFriendlyAddress(DUMMY_ADDRESS_HR);
+const DUMMY_ADDRESS_S = Uint8Array.from(DUMMY_ADDRESS.serialize());
+const BURN_ADDRESS  = Nimiq.Address.fromUserFriendlyAddress(BURN_ADDRESS_HR);
+console.log(Uint8Array.from(DUMMY_ADDRESS.serialize()));
+const RAW_WALLETS: WalletInfo[] = [
+    new WalletInfo('1ee3d926a49c', 'Main',
+        new Map<string, AccountInfo>([
+            [
+                DUMMY_ADDRESS_HR,
+                new AccountInfo(
+                    'm/0\'',
+                    'MyAccount',
+                    DUMMY_ADDRESS)],
+            ]),
+        [],
+        WalletType.BIP39),
+    new WalletInfo('1ee3d926a49d', 'Ledger',
+        new Map<string, AccountInfo>([
+            [
+                BURN_ADDRESS_HR,
+                new AccountInfo(
+                    'm/0\'',
+                    'MyLedger',
+                    BURN_ADDRESS)], ]),
+        [{ // ContractInfo
+            address: DUMMY_ADDRESS_S,
+            label: 'Savings',
+            ownerPath: 'm/0\'',
+            type: ContractType.VESTING,
+        }],
+        WalletType.LEDGER),
+    new WalletInfo('1ee3d926a49e', 'Old',
+        new Map<string, AccountInfo>([
+            [
+                BURN_ADDRESS_HR,
+                new AccountInfo(
+                    'm/0\'',
+                    'OldAccount',
+                    BURN_ADDRESS)],
+        ]),
+        [],
+        WalletType.LEGACY),
+];
+
+const DUMMIES = RAW_WALLETS.map(wallet => wallet.toObject());
+const DUMMY = DUMMIES[0];
+
+const beforeEachCallback = async () => {
+};
+
+const afterEachCallback = async () => {
+};
+
+describe('CookieJar', () => {
+
+    beforeEach(beforeEachCallback);
+
+    afterEach(afterEachCallback);
+
+    it('fill sets cookie', () => {
+        CookieJar.fill(DUMMIES);
+        const cookie = document.cookie;
+        expect(cookie).not.toBeNull();
+        expect(cookie.length).toBeGreaterThan(DUMMIES.length*50);
+        expect(cookie).toBe(CookieJar.encodeWallets(DUMMIES));
+        console.log(cookie);
+    });
+
+    it('fill produces same string', () => {
+        CookieJar.fill(DUMMIES);
+        const first = document.cookie;
+        CookieJar.fill(DUMMIES);
+        const second = document.cookie;
+        expect(first).toBe(second);
+        console.log(first.length, first);
+    });
+
+    it('encode and decode base 64', () => {
+        const test = Uint8Array.from(<number[]> 'tes'.split('').map(character => character.codePointAt(0)));
+        const encoded = CookieJar.base64Encode(test);
+        const decoded = CookieJar.base64Decode(encoded);
+        expect(decoded).toEqual(test);
+    });
+
+    it('encode and decode address', () => {
+        const address = DUMMY.accounts.values().next().value.address;
+        console.log(Array.from(address.values()).map(b => b.toString(2)).join(','));
+        const encoded = CookieJar.encodeAddress(address);
+        console.log(encoded);
+        expect(encoded.length).toBeLessThanOrEqual(21*4/3);
+        const decoded = CookieJar.decodeAddress(encoded);
+        console.log(Array.from(decoded.values()).map(b => b.toString(2)).join(','));
+        expect(decoded).toEqual(address);
+    });
+
+    it('encode and decode ID', () => {
+        expect(DUMMY.id).toBe(CookieJar.decodeId(CookieJar.encodeId(DUMMY.id)));
+        console.log(CookieJar.encodeId(DUMMY.id));
+    });
+
+    it('encode and decode type', () => {
+        const type = CookieJar.encodeType(DUMMY.type);
+        expect(type).toHaveLength(1);
+        expect(CookieJar.decodeType(type)).toBe(DUMMY.type);
+    });
+
+    it('Encode and decode reproduce source', () => {
+        const encoded = CookieJar.encodeWallets(DUMMIES);
+        const wallets = encoded.split(CookieJar.Separator.WALLET);
+        // https://stackoverflow.com/questions/2219526/how-many-bytes-in-a-javascript-string
+        console.log(`Serialized size: ${ encodeURI(encoded).split(/%(?:u[0-9A-F]{2})?[0-9A-F]{2}|./).length -1   }`)
+        // 215 for CookieJar 1
+        console.log(wallets.length);
+        console.log(wallets.join("\n---\n"));
+        expect(encoded.length).toBeGreaterThan(100);
+        expect(CookieJar.decodeWallets(encoded)).toEqual(DUMMIES);
+    });
+});
