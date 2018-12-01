491633c683d2232ecdcc140a65dd4ddfd757e2b8
sisou <hello@soerenschwert.de>
2018-12-01 10:34:20 -0600

    Simplify iframe.list() by throwing on non-standard key/migration situation

--- src/iframe.ts
+++ src/iframe.ts
@@ -1,7 +1,7 @@
 import { RpcServer } from '@nimiq/rpc';
 import { BrowserDetection } from '@nimiq/utils';
 import { WalletStore } from '@/lib/WalletStore';
-import { WalletInfoEntry } from '@/lib/WalletInfo';
+import { WalletInfoEntry, WalletInfo } from '@/lib/WalletInfo';
 import { CookieJar } from '@/lib/CookieJar';
 import { KeyguardClient } from '@nimiq/keyguard-client';
 
@@ -22,22 +22,24 @@ class IFrameApi {
         } else {
             wallets = await WalletStore.Instance.list();
         }
-
-        // If no wallets exist, see if the Keyguard has keys
-        if (wallets.length === 0) {
-            let keys: KeyguardRequest.KeyInfoObject[];
-            const client = new KeyguardClient();
-            keys = await client.list();
-
-            // If no keys exist, check legacy accounts and migrate
-            if (keys.length === 0) {
-                keys = await client.list(true);
-            }
-
-            // TODO Store wallets and trigger migration in Keyguard
+        if (wallets.length > 0) {
+            return wallets;
         }
 
-        return wallets;
+        // If no wallets exist, see if the Keyguard has keys
+        const client = new KeyguardClient();
+        const hasKeys = await client.hasKeys();
+        if (hasKeys) {
+            throw new Error('WALLETS_LOST');
+        }
+
+        // If no keys exist, check legacy accounts
+        const hasLegacyKeys = await client.hasKeys(true);
+        if (hasLegacyKeys) {
+            throw new Error('MIGRATION_REQUIRED');
+        }
+
+        return [];
     }
 
     private static get allowedOrigin(): string {
