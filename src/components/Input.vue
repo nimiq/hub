68329daddf4151c0ede90268408e8f6158adf1d7
sisou <hello@soerenschwert.de>
2018-12-10 23:59:44 -0600

    Adapt Checkout account selector to new design

---
+++ src/components/Input.vue
@@ -0,0 +1,10 @@
+<script lang="ts">
+import { Component, Prop, Vue } from 'vue-property-decorator';
+import { Input as StandardInput } from '@nimiq/vue-components';
+import { LABEL_MAX_LENGTH } from '@/lib/Constants';
+
+@Component
+export default class Input extends StandardInput {
+        @Prop({ default: LABEL_MAX_LENGTH }) protected maxBytes?: number;
+}
+</script>
--- src/views/Checkout.vue
+++ src/views/Checkout.vue
@@ -1,35 +1,61 @@
 <template>
     <div class="container">
-        <PaymentInfoLine v-if="rpcState"
-                         :amount="request.value"
-                         :networkFee="request.fee"
-                         :networkFeeEditable="false"
-                         :origin="rpcState.origin"/>
         <SmallPage>
-            <router-view @add-wallet="addWallet"/>
+            <PaymentInfoLine v-if="rpcState"
+                :amount="request.value"
+                :fee="request.fee"
+                :address="request.recipient.toUserFriendlyAddress()"
+                :origin="rpcState.origin"
+                :shopLogoUrl="request.shopLogoUrl"
+                @merchant-info-clicked="showMerchantInfo = true"
+            />
+
+            <h1 class="nq-h1">Choose an account to pay</h1>
+
+            <AccountSelector
+                :wallets="wallets"
+                :minBalance="request.value + request.fee"
+                @account-selected="accountSelected"
+                @login="login"/>
+
+            <Loader v-if="height === 0 || !hasBalances" status="Updating your balances..."/>
+
+            <AccountInfoScreen v-if="showMerchantInfo"
+                :address="request.recipient.toUserFriendlyAddress()"
+                :origin="rpcState.origin"
+                :shopLogoUrl="request.shopLogoUrl"
+                @close="showMerchantInfo = false"
+            />
         </SmallPage>
+
         <button class="global-close nq-button-s" :class="{'hidden': $route.name === 'checkout-success'}" @click="close">
             <span class="nq-icon arrow-left"></span>
             Cancel Payment
         </button>
+
+        <Network ref="network" :visible="false" @head-change="onHeadChange"/>
     </div>
 </template>
 
 <script lang="ts">
-import { Component, Emit, Vue } from 'vue-property-decorator';
-import { PaymentInfoLine, SmallPage } from '@nimiq/vue-components';
-import { ParsedCheckoutRequest, RequestType } from '../lib/RequestTypes';
+import { Component, Emit, Watch, Vue } from 'vue-property-decorator';
+import { PaymentInfoLine, AccountSelector, AccountInfo as AccountInfoScreen, SmallPage } from '@nimiq/vue-components';
+import { ParsedCheckoutRequest, RequestType, LoginResult } from '@/lib/RequestTypes';
 import { State as RpcState } from '@nimiq/rpc';
-import staticStore, { Static } from '../lib/StaticStore';
-import { LoginResult } from '@/lib/RequestTypes';
+import staticStore, { Static } from '@/lib/StaticStore';
 import { WalletStore } from '@/lib/WalletStore';
-import { WalletInfo } from '../lib/WalletInfo';
-import { Mutation } from 'vuex-class';
+import { AccountInfo } from '@/lib/AccountInfo';
+import { WalletInfo } from '@/lib/WalletInfo';
+import { State, Mutation } from 'vuex-class';
+import { TX_VALIDITY_WINDOW } from '@/lib/Constants';
+import Network from '@/components/Network.vue';
+import Loader from '@/components/Loader.vue';
 
-@Component({components: {PaymentInfoLine, SmallPage}})
+@Component({components: {PaymentInfoLine, AccountSelector, AccountInfoScreen, SmallPage, Network, Loader}})
 export default class Checkout extends Vue {
     @Static private rpcState!: RpcState;
     @Static private request!: ParsedCheckoutRequest;
+    @State private wallets!: WalletInfo[];
 
     @Mutation('addWallet') private $addWallet!: (walletInfo: WalletInfo) => any;
     @Mutation('setActiveAccount') private $setActiveAccount!: (payload: {
@@ -37,7 +63,132 @@ export default class Checkout extends Vue {
         userFriendlyAddress: string,
     }) => any;
 
-    private addWallet() {
+    private showMerchantInfo: boolean = false;
+    private height: number = 0;
+    private hasBalances: boolean = false;
+
+    private mounted() {
+        console.log(this.request);
+        this.getHeight();
+        this.getBalances();
+    }
+
+    private async getHeight() {
+        try {
+            await this.setHeightFromApi();
+        } catch (e) {
+            console.error(e);
+        }
+    }
+
+    private async getBalances() {
+        const network = (this.$refs.network as Network);
+        await network.connect();
+
+        const balancePromises: Array<Promise<WalletInfo>> = this.wallets.map((wallet: WalletInfo) => {
+            const addresses = Array.from(wallet.accounts.keys());
+
+            return network.getBalances(addresses).then((balances: Map<string, number>) => {
+                for (const address of addresses) {
+                    const accountInfo = wallet.accounts.get(address)!;
+                    accountInfo.balance = Nimiq.Policy.coinsToSatoshis(balances.get(address)!);
+                    wallet.accounts.set(address, accountInfo);
+                }
+                return wallet;
+            });
+        });
+
+        const wallets = await Promise.all(balancePromises);
+
+        for (const wallet of wallets) {
+            // Update IndexedDB
+            await WalletStore.Instance.put(wallet);
+
+            // Update Vuex
+            this.$addWallet(wallet);
+        }
+        this.hasBalances = true;
+    }
+
+    @Watch('height')
+    private logHeightChange(height: number, oldHeight: number) {
+        console.debug(`Got height: ${height} (was ${oldHeight})`);
+    }
+
+    private async setHeightFromApi() {
+        const raw = await fetch('https://test-api.nimiq.watch/latest/1');
+        const result = await raw.json();
+        this.height = result[0].height;
+    }
+
+    private onHeadChange(head: Nimiq.BlockHeader) {
+        this.height = head.height;
+    }
+
+    private accountSelected(walletId: string, address: string) {
+        if (!this.height) return; // TODO: Make it impossible for user to click when height is not ready
+
+        const walletInfo = this.wallets.find((wallet: WalletInfo) => wallet.id === walletId);
+        if (!walletInfo) {
+            console.error('Selected Wallet not found:', walletId);
+            return;
+        }
+
+        const accountInfo = walletInfo.accounts.get(address);
+        if (!accountInfo) {
+            console.error('Selected AccountInfo not found:', address);
+            return;
+        }
+
+        this.$store.commit('setActiveAccount', {
+            walletId: walletInfo.id,
+            userFriendlyAddress: accountInfo.userFriendlyAddress,
+        });
+
+        this.proceedToKeyguard(walletInfo, accountInfo);
+    }
+
+    private proceedToKeyguard(walletInfo: WalletInfo, accountInfo: AccountInfo) {
+        // The next block is the earliest for which tx are accepted by standard miners
+        const validityStartHeight = this.height + 1
+            - TX_VALIDITY_WINDOW
+            + this.request.validityDuration;
+
+        const request: KeyguardRequest.SignTransactionRequest = {
+            layout: 'checkout',
+            shopOrigin: this.rpcState.origin,
+            appName: this.request.appName,
+            shopLogoUrl: this.request.shopLogoUrl,
+
+            keyId: walletInfo.id,
+            keyPath: accountInfo.path,
+            keyLabel: walletInfo.label,
+
+            sender: accountInfo.address.serialize(),
+            senderType: Nimiq.Account.Type.BASIC,
+            senderLabel: accountInfo.label,
+            recipient: this.request.recipient.serialize(),
+            recipientType: this.request.recipientType,
+            // recipientLabel: '', // Checkout is using the shopOrigin instead
+            value: this.request.value,
+            fee: this.request.fee || 0,
+            validityStartHeight,
+            data: this.request.data,
+            flags: this.request.flags,
+        };
+
+        const storedRequest = Object.assign({}, request, {
+            sender: Array.from(request.sender),
+            recipient: Array.from(request.recipient),
+            data: Array.from(request.data!),
+        });
+        staticStore.keyguardRequest = storedRequest;
+
+        const client = this.$rpc.createKeyguardClient();
+        client.signTransaction(request).catch(console.error); // TODO: proper error handling
+    }
+
+    private login() {
         // Redirect to import
         const request: KeyguardRequest.ImportRequest = {
             appName: this.request.appName,
@@ -52,6 +203,7 @@ export default class Checkout extends Vue {
     }
 
     private async created() {
+        // Check if we are returning from the login request
         if (staticStore.sideResult && !(staticStore.sideResult instanceof Error)) {
             const sideResult = staticStore.sideResult as LoginResult;
             // Add imported wallet to store and pre-select wallet (if regular wallet)
@@ -76,3 +228,28 @@ export default class Checkout extends Vue {
     }
 }
 </script>
+
+<style scoped>
+    .small-page {
+        position: relative;
+        height: 70rem;
+    }
+
+    .nq-h1 {
+        margin-bottom: 1rem;
+        line-height: 1;
+        text-align: center;
+    }
+
+    .loader {
+        height: 25rem;
+        z-index: unset;
+        flex-shrink: 0;
+    }
+
+    .account-info {
+        position: absolute;
+        left: 0;
+        top: 0;
+    }
+</style>
