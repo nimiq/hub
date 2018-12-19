83990c6368bdb11f493ca3f3f9c6a2495728ae6d
Don Maxito <maxharz@gmail.com>
2018-12-19 14:11:35 +0100

    add request choose-address (wip)

--- client/AccountsClient.ts
+++ client/AccountsClient.ts
@@ -25,6 +25,7 @@ import {
     SimpleResult,
     ListResult,
     RpcResult,
+    ChooseAddressResult,
 } from '../src/lib/RequestTypes';
 
 export default class AccountsClient {
@@ -87,6 +88,11 @@ export default class AccountsClient {
 
     public login(request: SimpleRequest, requestBehavior = this._defaultBehavior): Promise<OnboardingResult> {
         return this._request(requestBehavior, RequestType.LOGIN, [request]);
+    }
+
+    public chooseAddress(request: SimpleRequest, requestBehavior = this._defaultBehavior)
+        : Promise<ChooseAddressResult> {
+        return this._request(requestBehavior, RequestType.CHOOSE_ADDRESS, [request]);
     }
 
     public signTransaction(
