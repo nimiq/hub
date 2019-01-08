9730e87441dc7d685e542938003a856e8d1760cc
sisou <hello@soerenschwert.de>
2019-01-08 13:28:36 +0100

    Address minor code review remarks

--- src/components/Input.vue
+++ src/components/Input.vue
@@ -5,6 +5,6 @@ import { LABEL_MAX_LENGTH } from '@/lib/Constants';
 
 @Component
 export default class Input extends StandardInput {
-        @Prop({ default: LABEL_MAX_LENGTH }) protected maxBytes?: number;
+        @Prop({ default: LABEL_MAX_LENGTH }) protected maxBytes!: number;
 }
 </script>
--- src/lib/RequestTypes.ts
+++ src/lib/RequestTypes.ts
@@ -312,7 +312,7 @@ export class AccountsRequest {
                 // Typescript cannot infer what type the request variable is from the control flow,
                 // thus we need to force-cast it here:
                 request = request as CheckoutRequest;
-                if (request.shopLogoUrl && state.origin && new URL(request.shopLogoUrl).origin !== state.origin) {
+                if (request.shopLogoUrl && new URL(request.shopLogoUrl).origin !== state.origin) {
                     throw new Error(
                         'shopLogoUrl must have same origin as caller website. Image at ' +
                         request.shopLogoUrl +
