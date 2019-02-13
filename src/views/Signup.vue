931603ec05886aed1d98ab6dcdcb082d64c6b362
sisou <hello@soerenschwert.de>
2019-02-13 12:08:04 -0600

    Fix request restart race-condition in Signup

--- src/views/Signup.vue
+++ src/views/Signup.vue
@@ -5,12 +5,16 @@ import { Component, Vue } from 'vue-property-decorator';
 import { ParsedSignupRequest } from '../lib/RequestTypes';
 import { Static } from '../lib/StaticStore';
 import { DEFAULT_KEY_PATH } from '@/lib/Constants';
+import { State } from 'vuex-class';
 
 @Component
 export default class Signup extends Vue {
     @Static private request!: ParsedSignupRequest;
+    @State private keyguardResult?: KeyguardRequest.CreateResult;
 
-    public mounted() {
+    public created() {
+        if (this.keyguardResult) return;
+
         const request: KeyguardRequest.CreateRequest = {
             appName: this.request.appName,
             defaultKeyPath: DEFAULT_KEY_PATH,
