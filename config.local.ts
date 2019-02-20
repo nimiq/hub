3a8a6046d88d5014d6642da5443045f726c4f01d
sisou <hello@soerenschwert.de>
2019-02-19 21:57:03 -0600

    Fix local dev config for mobile testing

--- config.local.ts
+++ config.local.ts
@@ -1,6 +1,6 @@
 import { NETWORK_TEST } from './src/lib/Constants';
 
 export default {
-    keyguardEndpoint: 'http://localhost:8000/src',
+    keyguardEndpoint: window.location.protocol + '//' + window.location.hostname + ':8000/src',
     network: NETWORK_TEST,
 };
