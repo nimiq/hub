bce4a4718160ef6e481e443021ea43b8d9499ca1
sisou <hello@soerenschwert.de>
2019-03-20 16:16:20 +0100

    Allow access to iframe from * in local environment

--- config.local.ts
+++ config.local.ts
@@ -4,5 +4,5 @@ export default {
     keyguardEndpoint: window.location.protocol + '//' + window.location.hostname + ':8000/src',
     network: NETWORK_TEST,
     networkEndpoint: 'https://network.nimiq-testnet.com',
-    privilegedOrigins: [ window.location.protocol + '//' + window.location.hostname + ':8001' ],
+    privilegedOrigins: [ '*' ],
 };
