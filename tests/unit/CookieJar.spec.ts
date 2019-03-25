f298439ea9e139524c5d2b76e188ca9bd7326abf
sisou <hello@soerenschwert.de>
2019-03-25 14:23:16 +0100

    Fix tests

--- tests/unit/CookieJar.spec.ts
+++ tests/unit/CookieJar.spec.ts
@@ -177,7 +177,7 @@ const OUT_DUMMY_WALLET_OBJECTS: WalletInfoEntry[] = [
                 DUMMY_ADDRESS_HR2,
                 {
                     path: 'not public',
-                    label: 'Standard Address',
+                    label: '',
                     address: DUMMY_ADDRESS_S2,
                 },
             ],
@@ -190,7 +190,7 @@ const OUT_DUMMY_WALLET_OBJECTS: WalletInfoEntry[] = [
     },
     {
         id: '1ee3d926a49d',
-        label: 'Ledger Account',
+        label: '',
         accounts: new Map<string, AccountInfoEntry>([
             [
                 BURN_ADDRESS_HR,
@@ -242,7 +242,7 @@ const OUT_DUMMY_WALLET_OBJECTS: WalletInfoEntry[] = [
                 DUMMY_ADDRESS_HR2,
                 {
                     path: 'not public',
-                    label: 'Standard Address',
+                    label: '',
                     address: DUMMY_ADDRESS_S2,
                 },
             ],
@@ -255,7 +255,7 @@ const OUT_DUMMY_WALLET_OBJECTS: WalletInfoEntry[] = [
     },
     {
         id: '1ee3d926a4a0',
-        label: 'Ledger Account',
+        label: '',
         accounts: new Map<string, AccountInfoEntry>([
             [
                 BURN_ADDRESS_HR,
