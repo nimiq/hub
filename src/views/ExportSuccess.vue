3dedb9fca7c0e2552065a5e2ab7146defabb1a31
Sebastian <sebastian@nimiq.com>
2019-04-01 16:11:49 +0200

    different success Messages for the different export results.

--- src/views/ExportSuccess.vue
+++ src/views/ExportSuccess.vue
@@ -4,7 +4,7 @@
             <Loader state="success">
                 <template slot="success">
                     <div class="success nq-icon"></div>
-                    <h1 class="title nq-h1">Account backed up!</h1>
+                    <h1 class="title nq-h1">{{successMessage}}</h1>
                 </template>
             </Loader>
         </SmallPage>
@@ -28,11 +28,23 @@ export default class ExportSuccess extends Vue {
     @Static private request!: ParsedSimpleRequest;
     @State private keyguardResult!: KeyguardClient.ExportResult;
 
+    private successMessage = '';
+
     private async mounted() {
         const wallet = (await WalletStore.Instance.get(this.request.walletId))!;
 
         wallet.fileExported = wallet.fileExported || this.keyguardResult.fileExported;
         wallet.wordsExported = wallet.wordsExported || this.keyguardResult.wordsExported;
+
+        if (this.keyguardResult.fileExported) {
+            if (this.keyguardResult.wordsExported) {
+                this.successMessage = 'Account backed up!';
+            } else {
+                this.successMessage = 'Login File exported!';
+            }
+        } else if (this.keyguardResult.wordsExported) {
+            this.successMessage = 'Recovery Words exported!';
+        }
 
         await WalletStore.Instance.put(wallet);
 
