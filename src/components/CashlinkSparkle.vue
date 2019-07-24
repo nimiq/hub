0ddb746b0028e9fca9311c88caa7f39ed006e0bb
sisou <hello@soerenschwert.de>
2019-07-24 14:43:28 +0200

    Start layouting receive screen

---
+++ src/components/CashlinkSparkle.vue
@@ -0,0 +1,61 @@
+<template>
+    <div class="cashlink-sparkle">
+        <svg class="desktop" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 252 240" width="100%" height="100%"><defs><radialGradient id="rg-1" cx="56.82" cy="150.3" r="5.17" gradientUnits="userSpaceOnUse"><stop offset="0" stop-color="#265dd7"/><stop offset="1" stop-color="#0582ca"/></radialGradient><radialGradient id="rg-2" cx="170.32" cy="200.8" r="5.17" xlink:href="#rg-1"/><radialGradient id="rg-3" cx="234.82" cy="89.3" r="5.17" xlink:href="#rg-1"/><radialGradient id="rg-4" cx="166.82" cy="21.3" r="5.17" xlink:href="#rg-1"/><radialGradient id="rg-5" cx="62.32" cy="47.3" r="5.17" xlink:href="#rg-1"/><radialGradient id="rg-6" cx="58.82" cy="206.3" r="5.17" gradientUnits="userSpaceOnUse"><stop offset="0" stop-color="#41a38e"/><stop offset="1" stop-color="#21bca5"/></radialGradient><radialGradient id="rg-7" cx="167.32" cy="235.3" r="5.17" xlink:href="#rg-6"/><radialGradient id="rg-8" cx="203.32" cy="164.3" r="5.17" xlink:href="#rg-6"/><radialGradient id="rg-9" cx="251.32" cy="123.3" r="5.17" xlink:href="#rg-6"/><radialGradient id="rg-10" cx="192.82" cy="92.8" r="5.17" xlink:href="#rg-6"/><radialGradient id="rg-11" cx="183.82" cy="52.8" r="5.17" xlink:href="#rg-6"/><radialGradient id="rg-12" cx="137.82" cy="34.8" r="5.17" xlink:href="#rg-6"/><radialGradient id="rg-13" cx="196.82" cy="206.3" r="5.17" gradientUnits="userSpaceOnUse"><stop offset="0" stop-color="#ec991c"/><stop offset="1" stop-color="#e9b213"/></radialGradient><radialGradient id="rg-14" cx="112.82" cy="210.8" r="5.17" xlink:href="#rg-13"/><radialGradient id="rg-15" cx="31.82" cy="206.8" r="5.17" xlink:href="#rg-13"/><radialGradient id="rg-16" cx="4.32" cy="143.3" r="5.17" xlink:href="#rg-13"/><radialGradient id="rg-17" cx="107.82" cy="46.8" r="5.17" xlink:href="#rg-13"/><radialGradient id="rg-18" cx="165.32" cy="64.8" r="5.17" xlink:href="#rg-13"/></defs><title>sparkle-desktop</title><g id="sparkle"><circle class="cls-2" cx="13.5" cy="107" r="2.5"/><circle class="cls-3" cx="6.5" cy="92" r="2.5"/><circle class="cls-2" cx="18.5" cy="87.5" r="2.5"/><circle class="cls-4" cx="27" cy="94.5" r="2.5"/><circle class="cls-5" cx="35.5" cy="99.5" r="2.5"/><circle class="cls-4" cx="22" cy="124" r="2.5"/><circle class="cls-4" cx="22" cy="134" r="2.5"/><circle class="cls-5" cx="32" cy="129" r="2.5"/><circle class="cls-6" cx="42.5" cy="114" r="2.5"/><circle class="cls-7" cx="52" cy="129" r="2.5"/><circle class="cls-7" cx="53" cy="139" r="2.5"/><circle class="cls-4" cx="28.5" cy="163.5" r="2.5"/><circle class="cls-5" cx="41.5" cy="168" r="2.5"/><circle class="cls-2" cx="22.5" cy="175" r="2.5"/><circle class="cls-4" cx="37.5" cy="181.5" r="2.5"/><circle class="cls-5" cx="46" cy="176.5" r="2.5"/><circle class="cls-6" cx="60.5" cy="179.5" r="2.5"/><circle class="cls-7" cx="63.5" cy="166.5" r="2.5"/><circle class="cls-7" cx="69.5" cy="174.5" r="2.5"/><circle class="cls-6" cx="67.5" cy="187" r="2.5"/><circle class="cls-7" cx="92.5" cy="192.5" r="2.5"/><circle class="cls-4" cx="82" cy="221" r="2.5"/><circle class="cls-5" cx="101" cy="217" r="2.5"/><circle class="cls-6" cx="101.5" cy="207" r="2.5"/><circle class="cls-7" cx="121" cy="200.5" r="2.5"/><circle class="cls-6" cx="131" cy="210.5" r="2.5"/><circle class="cls-7" cx="140.5" cy="199" r="2.5"/><circle class="cls-5" cx="150.5" cy="217" r="2.5"/><circle class="cls-7" cx="159.5" cy="192.5" r="2.5"/><circle class="cls-4" cx="178.5" cy="216" r="2.5"/><circle class="cls-4" cx="187" cy="210.5" r="2.5"/><circle class="cls-6" cx="184.5" cy="186.5" r="2.5"/><circle class="cls-7" cx="182.5" cy="174" r="2.5"/><circle class="cls-5" cx="200" cy="184.5" r="2.5"/><circle class="cls-2" cx="206.5" cy="207" r="2.5"/><circle class="cls-4" cx="208.5" cy="189.5" r="2.5"/><circle class="cls-2" cx="213" cy="199.5" r="2.5"/><circle class="cls-3" cx="222" cy="205" r="2.5"/><circle class="cls-2" cx="219.5" cy="192" r="2.5"/><circle class="cls-5" cx="210.5" cy="167.5" r="2.5"/><circle class="cls-6" cx="197" cy="171.5" r="2.5"/><circle class="cls-7" cx="188" cy="166.5" r="2.5"/><circle class="cls-4" cx="223" cy="163.5" r="2.5"/><circle class="cls-2" cx="229" cy="174.5" r="2.5"/><circle class="cls-3" cx="238" cy="179.5" r="2.5"/><circle class="cls-3" cx="241.5" cy="170.5" r="2.5"/><circle class="cls-4" cx="226.5" cy="154" r="2.5"/><circle class="cls-5" cx="217" cy="148.5" r="2.5"/><circle class="cls-6" cx="208" cy="143.5" r="2.5"/><circle class="cls-6" cx="209.5" cy="134" r="2.5"/><circle class="cls-7" cx="199.5" cy="129" r="2.5"/><circle class="cls-4" cx="229.5" cy="124" r="2.5"/><circle class="cls-2" cx="240" cy="126.5" r="2.5"/><circle class="cls-5" cx="218" cy="109" r="2.5"/><circle class="cls-6" cx="207" cy="104.5" r="2.5"/><circle class="cls-2" cx="236" cy="97" r="2.5"/><circle class="cls-3" cx="245" cy="92" r="2.5"/><circle class="cls-6" cx="203.5" cy="95" r="2.5"/><circle class="cls-5" cx="208" cy="81" r="2.5"/><circle class="cls-6" cx="194" cy="77.5" r="2.5"/><circle class="cls-7" cx="185.5" cy="82.5" r="2.5"/><circle class="cls-6" cx="187.5" cy="70" r="2.5"/><circle class="cls-7" cx="198" cy="109.5" r="2.5"/><circle class="cls-5" cx="193" cy="192" r="2.5"/><circle class="cls-7" cx="167.5" cy="187.5" r="2.5"/><circle class="cls-5" cx="169.5" cy="210" r="2.5"/><circle class="cls-2" cx="32.5" cy="192" r="2.5"/><circle class="cls-2" cx="45.5" cy="207" r="2.5"/><circle class="cls-5" cx="66" cy="199" r="2.5"/><circle class="cls-6" cx="75" cy="193.5" r="2.5"/><circle class="cls-5" cx="74" cy="205" r="2.5"/><circle class="cls-3" cx="10" cy="170.5" r="2.5"/><circle class="cls-6" cx="172.5" cy="57" r="2.5"/><circle class="cls-6" cx="164" cy="51.5" r="2.5"/><circle class="cls-5" cx="164.5" cy="41" r="2.5"/><circle class="cls-7" cx="145.5" cy="55" r="2.5"/><circle class="cls-5" cx="145.5" cy="34.5" r="2.5"/><circle class="cls-4" cx="165" cy="30" r="2.5"/><circle class="cls-2" cx="174" cy="23" r="2.5"/><circle class="cls-2" cx="183" cy="28" r="2.5"/><circle class="cls-3" cx="200.5" cy="27.5" r="2.5"/><circle class="cls-3" cx="208" cy="33.5" r="2.5"/><circle class="cls-4" cx="155.5" cy="27" r="2.5"/><circle class="cls-3" cx="174.5" cy="12.5" r="2.5"/><circle class="cls-2" cx="145.5" cy="14" r="2.5"/><circle class="cls-4" cx="145.5" cy="24.5" r="2.5"/><circle class="cls-6" cx="115.5" cy="43" r="2.5"/><circle class="cls-5" cx="115.5" cy="33" r="2.5"/><circle class="cls-4" cx="125.5" cy="22.5" r="2.5"/><circle class="cls-3" cx="125.5" cy="2.5" r="2.5"/><circle class="cls-3" cx="145.5" cy="4" r="2.5"/><circle class="cls-4" cx="96" cy="27" r="2.5"/><circle class="cls-5" cx="86.5" cy="41" r="2.5"/><circle class="cls-7" cx="97" cy="58.5" r="2.5"/><circle class="cls-6" cx="78.5" cy="57" r="2.5"/><circle class="cls-5" cx="69.5" cy="51" r="2.5"/><circle class="cls-5" cx="62" cy="57.5" r="2.5"/><circle class="cls-4" cx="53" cy="52" r="2.5"/><circle class="cls-7" cx="80" cy="68.5" r="2.5"/><circle class="cls-6" cx="63.5" cy="70" r="2.5"/><circle class="cls-5" cx="55" cy="64.5" r="2.5"/><circle class="cls-7" cx="66" cy="82.5" r="2.5"/><circle class="cls-6" cx="52" cy="86" r="2.5"/><circle class="cls-6" cx="48" cy="95" r="2.5"/><circle class="cls-8" cx="56.5" cy="100" r="2.5"/><circle class="cls-7" cx="53.5" cy="109.5" r="2.5"/><circle class="cls-4" cx="35" cy="76" r="2.5"/><circle class="cls-8" cx="27" cy="69.5" r="2.5"/><circle class="cls-3" cx="13.5" cy="73.5" r="2.5"/><circle class="cls-2" cx="32.5" cy="61" r="2.5"/><circle class="cls-3" cx="23.5" cy="56" r="2.5"/><circle class="cls-4" cx="77.5" cy="34.5" r="2.5"/><circle class="cls-2" cx="77.5" cy="23" r="2.5"/><circle class="cls-2" cx="68.5" cy="28" r="2.5"/><circle class="cls-3" cx="68" cy="17" r="2.5"/><circle class="cls-2" cx="86.5" cy="19.5" r="2.5"/><circle class="cls-6" cx="135.5" cy="43" r="2.5"/><circle class="cls-4" cx="191" cy="45.5" r="2.5"/><circle class="cls-4" cx="205.5" cy="59.5" r="2.5"/><circle class="cls-4" cx="183" cy="39.5" r="2.5"/><circle class="cls-5" cx="87.5" cy="212" r="2.5"/><circle class="cls-2" cx="69" cy="225" r="2.5"/><circle class="cls-3" cx="51.5" cy="225.5" r="2.5"/><circle class="cls-3" cx="44" cy="219.5" r="2.5"/><circle class="cls-2" cx="87" cy="233.5" r="2.5"/><circle class="cls-4" cx="106.5" cy="228.5" r="2.5"/><circle class="cls-5" cx="136.5" cy="220" r="2.5"/><circle class="cls-4" cx="126.5" cy="230.5" r="2.5"/><circle class="cls-2" cx="174.5" cy="230" r="2.5"/><circle class="cls-3" cx="184" cy="236" r="2.5"/><circle class="cls-4" cx="46.5" cy="193.5" r="2.5"/><circle class="cls-4" cx="69" cy="213.5" r="2.5"/><circle class="cls-9" cx="55" cy="148.5" r="2.5"/><circle class="cls-10" cx="168.5" cy="199" r="2.5"/><circle class="cls-11" cx="233" cy="87.5" r="2.5"/><circle class="cls-12" cx="165" cy="19.5" r="2.5"/><circle class="cls-13" cx="60.5" cy="45.5" r="2.5"/><circle class="cls-14" cx="57" cy="204.5" r="2.5"/><circle class="cls-15" cx="165.5" cy="233.5" r="2.5"/><circle class="cls-16" cx="201.5" cy="162.5" r="2.5"/><circle class="cls-17" cx="249.5" cy="121.5" r="2.5"/><circle class="cls-18" cx="191" cy="91" r="2.5"/><circle class="cls-19" cx="182" cy="51" r="2.5"/><circle class="cls-20" cx="136" cy="33" r="2.5"/><circle class="cls-21" cx="195" cy="204.5" r="2.5"/><circle class="cls-22" cx="111" cy="209" r="2.5"/><circle class="cls-23" cx="30" cy="205" r="2.5"/><circle class="cls-24" cx="2.5" cy="141.5" r="2.5"/><circle class="cls-25" cx="106" cy="45" r="2.5"/><circle class="cls-26" cx="163.5" cy="63" r="2.5"/></g></svg>
+
+        <div class="icon nq-blue-bg">
+            <img src='data:image/svg+xml,<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 128 128" width="100%" height="100%" fill="none" stroke="white" stroke-linecap="round" stroke-width="5px" stroke-linejoin="round"><path class="cls-1" d="M80.5,46.4v-.9a13,13,0,0,0-13-13h-7a13,13,0,0,0-13,13v13a13,13,0,0,0,13,13h4"/><path class="cls-1" d="M47.5,81.4v1.1a13,13,0,0,0,13,13h7a13,13,0,0,0,13-13v-13a13,13,0,0,0-13-13h-4"/><line class="cls-2" x1="64" y1="31.5" x2="64" y2="22.5"/><line class="cls-2" x1="64" y1="105.5" x2="64" y2="96.5"/></svg>'>
+        </div>
+    </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+
+@Component
+export default class CashlinkSparkle extends Vue {}
+</script>
+
+<style scoped>
+    .cashlink-sparkle {
+        position: relative;
+        width: 31.5rem; /* 252px */
+        height: 30rem; /* 240px */
+    }
+
+    .icon {
+        position: absolute;
+        left: 7.875rem;
+        top: 7.75rem;
+        width: 16rem;
+        height: 16rem;
+        border-radius: 8rem;
+    }
+
+    .cls-2, .cls-3, .cls-4, .cls-5, .cls-6, .cls-7 { fill: var(--nimiq-blue); }
+    .cls-2  { opacity: 0.12; }
+    .cls-3  { opacity: 0.1; }
+    .cls-4  { opacity: 0.14; }
+    .cls-5  { opacity: 0.16; }
+    .cls-6  { opacity: 0.18; }
+    .cls-7  { opacity: 0.2; }
+
+    .cls-8  { fill: var(--nimiq-green); }
+    .cls-9  { fill: url(#rg-1); }
+    .cls-10 { fill: url(#rg-2); }
+    .cls-11 { fill: url(#rg-3); }
+    .cls-12 { fill: url(#rg-4); }
+    .cls-13 { fill: url(#rg-5); }
+    .cls-14 { fill: url(#rg-6); }
+    .cls-15 { fill: url(#rg-7); }
+    .cls-16 { fill: url(#rg-8); }
+    .cls-17 { fill: url(#rg-9); }
+    .cls-18 { fill: url(#rg-10); }
+    .cls-19 { fill: url(#rg-11); }
+    .cls-20 { fill: url(#rg-12); }
+    .cls-21 { fill: url(#rg-13); }
+    .cls-22 { fill: url(#rg-14); }
+    .cls-23 { fill: url(#rg-15); }
+    .cls-24 { fill: url(#rg-16); }
+    .cls-25 { fill: url(#rg-17); }
+    .cls-26 { fill: url(#rg-18); }
+</style>
