nimiq/hub

commit ecdb651ae02dd5592b80d601fc344fbfb3af48f7

Prevent parallel calls to Network to result in the second failing
The second fails because the NetworkClient.Instance is already set after the first call, but internally the NetworkClient is still awaiting the RpcClient's init() call. But because the instance is already set, the Network.vue component does not wait for the second request, throwing "this._eventClient is undefined" or "Cannot read property call of undefined" errors.

src/components/Network.vue
@@ -17,6 +17,8 @@ import { VestingContractInfo } from '../lib/ContractInfo';
 @Component
 class Network extends Vue {
     private static _hasOrSyncsOnTopOfConsensus = false;
+    private _clientInitiatedPromise!: Promise<void>;
+    private _clientInitiatedResolver!: () => void;
 
     private boundListeners: Array<[NetworkClient.Events, (...args: any[]) => void]> = [];
 
@@ -191,6 +193,8 @@ class Network extends Vue {
     }
 
     private created() {
+        this._clientInitiatedPromise = new Promise((res) => this._clientInitiatedResolver = res);
+
         this.$on(Network.Events.CONSENSUS_ESTABLISHED, () => {
             Network._hasOrSyncsOnTopOfConsensus = true;
         });
@@ -210,7 +214,10 @@ class Network extends Vue {
         if (!NetworkClient.hasInstance()) {
             const networkClient = NetworkClient.createInstance(Config.networkEndpoint);
             await networkClient.init();
+            this._clientInitiatedResolver();
         }
+
+        await this._clientInitiatedPromise;
 
         if (this.boundListeners.length === 0) {
             this._registerNetworkListener(NetworkClient.Events.API_READY,

commit 1efdab5acfef0911ac0158eb7e1fbbe30fdab30c

Network: fix initialization of Network client
Fixes a bug introduced in https://github.com/nimiq/hub/pull/312
that results in calls to instances of the Network component to never
execute for instances that are not the first created. This bug
occured as the promise waiting for the client initialization was
not shared across Network component instances.

src/components/Network.vue
@@ -17,8 +17,6 @@ import { VestingContractInfo } from '../lib/ContractInfo';
 @Component
 class Network extends Vue {
     private static _hasOrSyncsOnTopOfConsensus = false;
-    private _clientInitiatedPromise!: Promise<void>;
-    private _clientInitiatedResolver!: () => void;
 
     private boundListeners: Array<[NetworkClient.Events, (...args: any[]) => void]> = [];
 
@@ -193,8 +191,6 @@ class Network extends Vue {
     }
 
     private created() {
-        this._clientInitiatedPromise = new Promise((res) => this._clientInitiatedResolver = res);
-
         this.$on(Network.Events.CONSENSUS_ESTABLISHED, () => {
             Network._hasOrSyncsOnTopOfConsensus = true;
         });
@@ -212,12 +208,10 @@ class Network extends Vue {
 
     private async _getNetworkClient(): Promise<NetworkClient> {
         if (!NetworkClient.hasInstance()) {
-            const networkClient = NetworkClient.createInstance(Config.networkEndpoint);
-            await networkClient.init();
-            this._clientInitiatedResolver();
+            NetworkClient.createInstance(Config.networkEndpoint);
         }
-
-        await this._clientInitiatedPromise;
+        // Make sure the client is initialized
+        await NetworkClient.Instance.init();
 
         if (this.boundListeners.length === 0) {
             this._registerNetworkListener(NetworkClient.Events.API_READY,