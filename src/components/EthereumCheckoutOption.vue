818d918d436409bcb2fe1d334f0d8b76da63ffde
Sebastian <sebastian@nimiq.com>
2019-09-04 17:50:33 -0600

    add EthereumCheckoutOption

---
+++ src/components/EthereumCheckoutOption.vue
@@ -0,0 +1,14 @@
+<script lang="ts">
+import { State, Mutation, Getter } from 'vuex-class';
+import bigInt from 'big-integer';
+import {
+    EtherDirectPaymentOptions,
+    ParsedEtherDirectPaymentOptions,
+} from '../lib/paymentOptions/EtherPaymentOptions';
+import NonNimiqCheckoutOption from './NonNimiqCheckoutOption.vue';
+
+export default class EtherCheckoutOption
+    extends NonNimiqCheckoutOption<ParsedEtherDirectPaymentOptions> {
+    protected icon = 'icon-eth.svg';
+}
+</script>
