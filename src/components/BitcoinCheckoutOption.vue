06d27d892ad4e2bb24d3fa52f39da9ee6fb63cce
Sebastian <sebastian@nimiq.com>
2019-09-04 17:50:18 -0600

    add BitcoinCheckoutOption

---
+++ src/components/BitcoinCheckoutOption.vue
@@ -0,0 +1,16 @@
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+import { State, Mutation, Getter } from 'vuex-class';
+import CheckoutOption from './CheckoutOption.vue';
+import {
+    BitcoinDirectPaymentOptions,
+    ParsedBitcoinDirectPaymentOptions,
+} from '../lib/paymentOptions/BitcoinPaymentOptions';
+import NonNimiqCheckoutOption from './NonNimiqCheckoutOption.vue';
+
+export default class BitcoinCheckoutOption
+    extends NonNimiqCheckoutOption<ParsedBitcoinDirectPaymentOptions> {
+    protected icon = 'icon-btc.svg';
+
+}
+</script>
