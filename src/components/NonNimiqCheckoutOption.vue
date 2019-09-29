2499473923ec2bb7593293595e5a1dc82b5142c4
Sebastian <sebastian@nimiq.com>
2019-09-29 17:03:07 +0200

    adaption of non-nimiq screen before currency selection

--- src/components/NonNimiqCheckoutOption.vue
+++ src/components/NonNimiqCheckoutOption.vue
@@ -41,13 +41,31 @@
                 :shopLogoUrl="request.shopLogoUrl"
                 :startTime="request.time"
                 :endTime="paymentOptions.expires" />
-            <h2 class="nq-h1">
-                Pay the amount to the shown address
+            <h2 class="nq-h1" v-if="this.selected">
+                Send your transaction
             </h2>
-            <PageBody>
+            <PageBody v-if="!this.selected">
+                <Account layout="column"
+                    :image="request.shopLogoUrl"
+                    :label="rpcState.origin.split('://')[1]"/>
+                <div class="amounts">
+                    <Amount class="crypto nq-light-blue"
+                        :currency="paymentOptions.currency"
+                        :totalDecimals="paymentOptions.digits"
+                        :minDecimals="0"
+                        :maxDecimals="paymentOptions.digits < 8 ? paymentOptions.digits : 8"
+                        :amount="paymentOptions.amount"
+                    />
+                    <Amount class="fiat"
+                        :currency="request.fiatCurrency.code"
+                        :totalDecimals="request.fiatCurrency.digits"
+                        :decimals="request.fiatCurrency.digits"
+                        :amount="request.fiatAmount * 10 ** request.fiatCurrency.digits"
+                    />
+                </div>
+            </PageBody>
+            <PageBody v-else>
                 <div class="qr-code">
-                    <QrCodeIcon />
-                    <div class="nq-blue">Payment Info will appear after confirmation</div>
                 </div>
                 <div class="copyable-payment-information">
                     <Copyable :text="paymentOptions.baseUnitAmount">
@@ -64,9 +82,12 @@
                     </Copyable>
                 </div>
             </PageBody>
-            <PageFooter>
-                <a v-if="selected" class="nq-button light-blue" :href="paymentOptions.paymentLink">Open wallet</a>
-                <button v-else class="nq-button light-blue" @click="selectCurrency">Select {{paymentOptions.currency}}</button>
+            <PageFooter v-if="selected">
+                <a class="nq-button light-blue" :href="paymentOptions.paymentLink">Use app</a>
+                <p class="nq-text-s"><AlertTriangleIcon/>Don’t close this window until confirmation</p>
+            </PageFooter>
+            <PageFooter v-else>
+                <button class="nq-button light-blue" @click="selectCurrency">Pay with {{paymentOptions.currency}}</button>
             </PageFooter>
         </SmallPage>
     </div>
@@ -74,11 +95,13 @@
 
 <script lang="ts">
 import { Component } from 'vue-property-decorator';
-import { Copyable,
+import {
+    Account,
+    AlertTriangleIcon,
+    Copyable,
     PageBody,
     PageFooter,
     PaymentInfoLine,
-    QrCodeIcon,
     SmallPage,
     StopwatchIcon,
     Amount,
@@ -91,12 +114,13 @@ import StatusScreen from './StatusScreen.vue';
 import CheckoutServerApi from '../lib/CheckoutServerApi';
 
 @Component({components: {
+    Account,
+    AlertTriangleIcon,
     Copyable,
     CurrencyInfo,
     PageBody,
     PageFooter,
     PaymentInfoLine,
-    QrCodeIcon,
     SmallPage,
     StatusScreen,
     StopwatchIcon,
@@ -119,6 +143,7 @@ export default class NonNimiqCheckoutOption<
     }
 
     protected async selectCurrency() {
+        this.selected = true;
         if (this.request.callbackUrl) {
             this.showStatusScreen = true;
             try {
@@ -132,7 +157,6 @@ export default class NonNimiqCheckoutOption<
             this.$rpc.reject(new Error('Failed to fetch recipient'));
             return;
         }
-
         let element: HTMLElement | null = document.getElementById(this.paymentOptions.currency!);
         if (element) {
             element = element.querySelector('.qr-code');
@@ -142,16 +166,19 @@ export default class NonNimiqCheckoutOption<
         }
 
         this.$emit('chosen', this.paymentOptions.currency);
-        this.selected = true;
 
-        QrCode.render({
-            text: this.paymentOptions.paymentLink,
-            radius: 0.5, // 0.0 to 0.5
-            ecLevel: 'M', // L, M, Q, H
-            fill: '#1F2348', // foreground color
-            background: null, // color or null for transparent
-            size: 200, // in pixels
-        }, element);
+        this.$nextTick(() =>
+                QrCode.render({
+                    text: this.paymentOptions.paymentLink,
+                    radius: 0.5, // 0.0 to 0.5
+                    ecLevel: 'M', // L, M, Q, H
+                    fill: '#1F2348', // foreground color
+                    background: null, // color or null for transparent
+                    size: 200, // in pixels
+                },
+                element!,
+            ),
+        );
 
         this.checkNetworkInterval = window.setInterval(async () => {
             if (!this.request.callbackUrl || !this.request.csrf) return;
@@ -228,43 +255,57 @@ export default class NonNimiqCheckoutOption<
         transition: filte .5s ease, opacity .5s ease;
     }
 
-    .payment-option .page-body .qr-code {
-        position: relative;
-    }
-
-    .payment-option .page-body >>> .nq-icon {
-        width: 25rem;
-        height: auto;
-        opacity: .1;
-        filter: blur(1.5rem);
-        transition: opacity .5s ease;
-    }
-
-    .payment-option .page-body .qr-code div {
-        display: flex;
-        position: absolute;
-        top: 0;
-        left: 0;
-        width: 100%;
-        height: 100%;
-        align-items: center;
-        justify-content: center;
-        text-align: center;
-        opacity: .4;
-        transition: opacity .5s ease;
-    }
-
-    .payment-option .page-body .qr-code >>> canvas {
-        position: absolute;
-        top: 0;
-        left: 0;
-        width: 100%;
-        height: 100%;
-    }
-
-    .payment-option.confirmed .page-body >>> .nq-icon,
-    .payment-option.confirmed .page-body .qr-code div  {
+    .payment-option:not(.confirmed) .info-line >>> .amounts,
+    .payment-option:not(.confirmed) .info-line >>> .arrow-runway,
+    .payment-option:not(.confirmed) .info-line >>> .description .account {
         opacity: 0;
+        transition: opacity .5s ease;
+    }
+
+    .payment-option .page-body {
+        width: 52.5rem;
+        padding-top: 0;
+        padding-bottom: 0;
+    }
+
+    .payment-option .account >>> .identicon-and-label .identicon {
+        width: 21rem;
+        height: 21rem;
+    }
+
+    .payment-option .account >>> .identicon-and-label .label {
+        font-weight: 600;
+        font-size: 3.5rem;
+        line-height: 3.5rem;
+        margin-top: 2.75rem;
+        margin-bottom: 3rem;
+    }
+
+    .payment-option .amounts {
+        display: flex;
+        flex-direction: column;
+        flex-grow: 1;
+        width: 100%;
+        align-items: center;
+        border-top: 0.125rem solid rgba(31, 35, 72, 0.1); /* based on nq-dark-blue */
+    }
+
+    .payment-option .amounts .crypto {
+        margin-top: 3.5rem;
+        font-weight: 600;
+        font-size: 5rem;
+        line-height: 5rem;
+    }
+
+    .payment-option .amounts .fiat {
+        margin-top: 1.75rem;
+        font-size: 2rem;
+        line-height: 2.75rem;
+    }
+
+    .payment-option.confirmed .page-body {
+        justify-content: flex-end;
+        padding-bottom: 1rem;
     }
 
     .payment-option .page-body .payment-link {
@@ -294,8 +335,21 @@ export default class NonNimiqCheckoutOption<
         pointer-events: none;
     }
 
-    a.nq-button {
+    .page-footer a.nq-button {
         line-height: 7.5rem;
+        margin: 2rem 4.75rem 2rem;
+    }
+
+    .page-footer a.nq-button + p.nq-text-s {
+        align-self: center;
+        color:  rgba(31, 35, 72, 0.5);
+        align-content: center;
+        margin: 0 0 1rem;
+        display: flex;
+    }
+
+    .page-footer a.nq-button + p.nq-text-s > svg {
+        margin-right: 1rem;
     }
 
     /* todo */
@@ -316,16 +370,30 @@ export default class NonNimiqCheckoutOption<
         border-radius: 0 0 .5rem .5rem;
     }
 
-    .copyable-payment-information >>> .copyable:before {
+    .copyable-payment-information >>> .copyable.copied:after,
+    .copyable-payment-information >>> .copyable:before,
+    .copyable-payment-information.copied >>> .copyable:before {
         content: "";
     }
 
-    .copyable-payment-information >>> .copyable.copied:after,
-    .copyable-payment-information >>> .copyable:not(.copied):hover:before {
+    .copyable-payment-information >>> .copyable:hover:before {
         padding: 0;
+        opacity: .5;
         background-image: url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 35 40'%3E%3Cpath fill-rule='evenodd' clip-rule='evenodd' d='M34.07 6.01L28.95.92c-.6-.59-1.4-.92-2.24-.92H12.33a3.17 3.17 0 0 0-3.16 3.17v5.16h-6A3.17 3.17 0 0 0 0 11.5v25.35A3.17 3.17 0 0 0 3.17 40h19.5a3.17 3.17 0 0 0 3.16-3.16v-5.17h6A3.17 3.17 0 0 0 35 28.5V8.25c0-.84-.33-1.65-.93-2.24zM22.5 35.83c0 .46-.37.84-.83.84H4.17a.83.83 0 0 1-.84-.84V12.5c0-.46.38-.83.84-.83h12.97c.22 0 .43.08.58.23l4.53 4.43c.16.16.25.37.25.6v18.9zm3.75-7.5h4.58c.46 0 .84-.37.84-.83V8.6a.83.83 0 0 0-.25-.6l-4.58-4.47a.83.83 0 0 0-.58-.24l-12.93.04a.83.83 0 0 0-.83.84v3.75c0 .23.19.41.42.41h4.63c.84 0 1.64.33 2.23.93l5.12 5.09c.6.59.93 1.4.93 2.23v11.34c0 .23.19.41.42.41z' fill='currentColor'/%3E%3C/svg%3E");
         left: 1.5rem;
-        top: .5rem;
+        top: calc(50% - 1rem);
+        width: 1.75rem;
+        height: 2rem;
+        transition: ease-out;
+    }
+
+    .copyable-payment-information >>> .copyable.copied:before {
+        padding: 0;
+        opacity: 1;
+
+        background-image: url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 35 40'%3E%3Cpath fill-rule='evenodd' clip-rule='evenodd' d='M34.07 6.01L28.95.92c-.6-.59-1.4-.92-2.24-.92H12.33a3.17 3.17 0 0 0-3.16 3.17v5.16h-6A3.17 3.17 0 0 0 0 11.5v25.35A3.17 3.17 0 0 0 3.17 40h19.5a3.17 3.17 0 0 0 3.16-3.16v-5.17h6A3.17 3.17 0 0 0 35 28.5V8.25c0-.84-.33-1.65-.93-2.24zM22.5 35.83c0 .46-.37.84-.83.84H4.17a.83.83 0 0 1-.84-.84V12.5c0-.46.38-.83.84-.83h12.97c.22 0 .43.08.58.23l4.53 4.43c.16.16.25.37.25.6v18.9zm3.75-7.5h4.58c.46 0 .84-.37.84-.83V8.6a.83.83 0 0 0-.25-.6l-4.58-4.47a.83.83 0 0 0-.58-.24l-12.93.04a.83.83 0 0 0-.83.84v3.75c0 .23.19.41.42.41h4.63c.84 0 1.64.33 2.23.93l5.12 5.09c.6.59.93 1.4.93 2.23v11.34c0 .23.19.41.42.41z' fill='currentColor'/%3E%3C/svg%3E");
+        left: 1.5rem;
+        top: calc(50% - 1rem);
         width: 1.75rem;
         height: 2rem;
     }
