1b61a944483857e10414331305849acb0e0277b0
sisou <hello@soerenschwert.de>
2019-10-07 08:31:25 +0200

    Use CloseButton component in manual copy screen

--- src/components/CheckoutManualPaymentDetails.vue
+++ src/components/CheckoutManualPaymentDetails.vue
@@ -1,8 +1,6 @@
 <template>
     <SmallPage class="checkout-manual-payment-details nq-blue-bg">
-        <button class="nq-button-s inverse close-button" @click="$emit(constructor.Events.CLOSE)">
-            <CloseIcon/>
-        </button>
+        <CloseButton class="top-right" @click="$emit(constructor.Events.CLOSE)"/>
         <p class="nq-notice info">
             Paste the information<br/>below into your<br/>wallet app.
         </p>
@@ -17,9 +15,9 @@
 
 <script lang="ts">
 import { Component, Prop, Vue } from 'vue-property-decorator';
-import { CloseIcon, CopyableField, SmallPage } from '@nimiq/vue-components';
+import { CloseButton, CopyableField, SmallPage } from '@nimiq/vue-components';
 
-@Component({components: {CloseIcon, CopyableField, SmallPage}})
+@Component({components: {CloseButton, CopyableField, SmallPage}})
 class CheckoutManualPaymentDetails extends Vue {
     @Prop({
         type: Array,
@@ -41,27 +39,6 @@ export default CheckoutManualPaymentDetails;
 <style scoped>
     .small-page {
         padding: 4rem;
-    }
-
-    .close-button {
-        align-self: flex-end;
-        font-size: 3rem;
-        line-height: 0;
-        padding: 0;
-        height: unset;
-        color: white;
-        background: transparent !important;
-    }
-
-    .close-button .nq-icon {
-        opacity: .2;
-        transition: opacity .3s var(--nimiq-ease);
-    }
-
-    .close-button:hover .nq-icon,
-    .close-button:focus .nq-icon,
-    .close-button:active .nq-icon {
-        opacity: .4;
     }
 
     .nq-notice {
