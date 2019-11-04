309566d607ee170bcf209ddc2652fa687b29c8d3
Daniel <github.danimoh@neverbox.com>
2019-11-04 19:43:24 +0100

    Update Checkout to use Carousel of CheckoutOptions

--- src/views/Checkout.vue
+++ src/views/Checkout.vue
@@ -1,359 +1,130 @@
 <template>
     <div class="container">
-        <SmallPage v-if="height === 0 || !hasBalances">
-            <StatusScreen title="Updating your balances" :status="status"/>
-        </SmallPage>
-
-        <SmallPage v-else>
-            <PaymentInfoLine v-if="rpcState"
-                :cryptoAmount="{
-                    amount: request.value,
-                    currency: 'NIM',
-                    decimals: 5,
-                }"
-                :address="request.recipient.toUserFriendlyAddress()"
-                :origin="rpcState.origin"
-                :shopLogoUrl="request.shopLogoUrl"
-            />
-
-            <h1 class="nq-h1">Choose an Address to pay</h1>
-
-            <div v-if="!hasSufficientBalanceAccount" class="non-sufficient-balance">
-                <p class="nq-text nq-orange">None of your Addresses has sufficient balance.</p>
-                <a class="nq-button-s nq-light-blue-bg" href="https://nimiq.com/#exchanges" target="_blank">
-                    <TransferIcon/> Get NIM&nbsp;
-                </a>
-            </div>
-
-            <AccountSelector
-                :wallets="processedWallets"
-                :minBalance="minBalance"
-                @account-selected="setAccountOrContract"
-                @login="() => goToOnboarding(false)"/>
-        </SmallPage>
+        <Carousel
+            :entries="request.paymentOptions.map((paymentOptions) => paymentOptions.currency)"
+            :animationDuration="500"
+            :selected="selectedCurrency"
+            :disabled="choosenCurrency !== null"
+            @select="updateSelection">
+            <template v-for="paymentOptions of request.paymentOptions" v-slot:[paymentOptions.currency]>
+                <NimiqCheckoutOption
+                    v-if="paymentOptions.currency === Currency.NIM"
+                    :paymentOptions="paymentOptions"
+                    :key="paymentOptions.currency"
+                    :class="{confirmed: choosenCurrency === paymentOptions.currency}"
+                    @chosen="chooseCurrency"/>
+                <EthereumCheckoutOption
+                    v-else-if="paymentOptions.currency === Currency.ETH"
+                    :paymentOptions="paymentOptions"
+                    :key="paymentOptions.currency"
+                    :class="{confirmed: choosenCurrency === paymentOptions.currency}"
+                    @chosen="chooseCurrency"/>
+                <BitcoinCheckoutOption
+                    v-else-if="paymentOptions.currency === Currency.BTC"
+                    :paymentOptions="paymentOptions"
+                    :key="paymentOptions.currency"
+                    :class="{confirmed: choosenCurrency === paymentOptions.currency}"
+                    @chosen="chooseCurrency"/>
+            </template>
+        </Carousel>
 
         <button class="global-close nq-button-s" @click="close">
-            <ArrowLeftSmallIcon/>
+            <ArrowLeftSmallIcon class="nq-blue-bg"/>
             Cancel Payment
         </button>
-
-        <Network ref="network" :visible="false" @head-change="onHeadChange"/>
     </div>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator';
-import { PaymentInfoLine, AccountSelector, SmallPage } from '@nimiq/vue-components';
-import { TransferIcon, ArrowLeftSmallIcon } from '@nimiq/vue-components';
+import { Carousel, ArrowLeftSmallIcon } from '@nimiq/vue-components';
 import { ParsedCheckoutRequest } from '../lib/RequestTypes';
-import { Account, Currency, RequestType } from '../lib/PublicRequestTypes';
+import BitcoinCheckoutOption from '../components/BitcoinCheckoutOption.vue';
+import EthereumCheckoutOption from '../components/EthereumCheckoutOption.vue';
+import NimiqCheckoutOption from '../components/NimiqCheckoutOption.vue';
+import { Currency } from '../lib/PublicRequestTypes';
 import { State as RpcState } from '@nimiq/rpc';
 import staticStore, { Static } from '../lib/StaticStore';
-import { WalletStore } from '../lib/WalletStore';
-import { AccountInfo } from '../lib/AccountInfo';
-import { WalletInfo, WalletType } from '../lib/WalletInfo';
-import { State, Mutation, Getter } from 'vuex-class';
-import {
-    TX_VALIDITY_WINDOW,
-    LEGACY_GROUPING_ACCOUNT_ID,
-    LEGACY_GROUPING_ACCOUNT_LABEL,
-    ERROR_CANCELED,
-} from '../lib/Constants';
-import Network from '../components/Network.vue';
-import StatusScreen from '../components/StatusScreen.vue';
-import KeyguardClient from '@nimiq/keyguard-client';
-import { ContractInfo, VestingContractInfo } from '../lib/ContractInfo';
+import { ERROR_CANCELED } from '../lib/Constants';
 
 @Component({components: {
-    PaymentInfoLine,
-    AccountSelector,
-    SmallPage,
-    Network,
-    StatusScreen,
-    TransferIcon,
     ArrowLeftSmallIcon,
+    Carousel,
+    BitcoinCheckoutOption,
+    EthereumCheckoutOption,
+    NimiqCheckoutOption,
 }})
 export default class Checkout extends Vue {
-    private static readonly BALANCE_CHECK_STORAGE_KEY = 'nimiq_checkout_last_balance_check';
 
     @Static private rpcState!: RpcState;
     @Static private request!: ParsedCheckoutRequest;
-    @State private wallets!: WalletInfo[];
-
-    @Getter private processedWallets!: WalletInfo[];
-    @Getter private findWalletByAddress!: (address: string, includeContracts: boolean) => WalletInfo | undefined;
-
-    @Mutation('addWallet') private $addWallet!: (walletInfo: WalletInfo) => any;
-    @Mutation('setActiveAccount') private $setActiveAccount!: (payload: {
-        walletId: string,
-        userFriendlyAddress: string,
-    }) => any;
-
-    private height: number = 0;
-    private hasBalances: boolean = false;
-    private status: string = 'Connecting to network...';
+    private choosenCurrency: Currency | null = null;
+    private selectedCurrency: Currency = Currency.NIM;
 
     private async created() {
         const $subtitle = document.querySelector('.logo .logo-subtitle')!;
         $subtitle.textContent = 'Checkout';
-
-        if (this.wallets.length === 0) {
-            this.goToOnboarding(true);
-            return;
-        }
-    }
-
-    private async mounted() {
-        // Requires Network child component to be rendered
-        this.addConsensusListeners();
-        const balances = await this.getBalances();
-
-        // Handle optional sender address included in the request
-        if (this.request.sender) {
-            let errorMsg = '';
-            // Check if the address exists
-            const senderAddress = this.request.sender.toUserFriendlyAddress();
-            const wallet = this.findWalletByAddress(senderAddress, true);
-            if (wallet) {
-                // Check if that address has enough balance
-                const balance = balances.get(senderAddress);
-                if (balance && Nimiq.Policy.coinsToSatoshis(balance) >= this.minBalance) {
-                    // Forward to Keyguard, skipping account selection
-                    this.setAccountOrContract(wallet.id, senderAddress, true);
-                    return;
-                } else {
-                    errorMsg = 'Address does not have sufficient balance';
-                }
-            } else {
-                errorMsg = 'Address not found';
-            }
-
-            if (this.request.forceSender) {
-                this.$rpc.reject(new Error(errorMsg));
-                return;
-            }
-        }
-
-        // Remove StatusScreen to unveil account selector
-        this.hasBalances = true;
-    }
-
-    private async getBalances(): Promise<Map<string, number>> {
-        const cache = this.getLastBalanceUpdateHeight();
-        const isRefresh = !window.performance || performance.navigation.type === 1;
-
-        const sideResultAddedWallet = !!staticStore.sideResult && !!(staticStore.sideResult as Account[]).length;
-        if (!sideResultAddedWallet && cache && !isRefresh) {
-            this.onHeadChange(cache);
-            return cache.balances;
-        }
-
-        // Copy wallets to be able to manipulate them
-        const wallets = this.wallets.slice(0);
-
-        // Generate a new array with references to the respective wallets' accounts
-        const accountsAndContracts = wallets.reduce((acc, wallet) => {
-            acc.push(...wallet.accounts.values());
-            acc.push(...wallet.contracts);
-            return acc;
-        }, [] as Array<AccountInfo | ContractInfo>);
-
-        // Reduce userfriendly addresses from that
-        const addresses = accountsAndContracts.map((accountOrContract) => accountOrContract.userFriendlyAddress);
-
-        // Get balances through pico consensus, also triggers head-change event
-        const network = (this.$refs.network as Network);
-        const balances: Map<string, number> = await network.getBalances(addresses);
-
-        // Update accounts/contracts with their balances
-        // (The accounts are still references to themselves in the wallets' accounts maps)
-        for (const accountOrContract of accountsAndContracts) {
-            const balance = balances.get(accountOrContract.userFriendlyAddress);
-            if (balance === undefined) continue;
-
-            if ('type' in accountOrContract && accountOrContract.type === Nimiq.Account.Type.VESTING) {
-                // Calculate available amount for vesting contract
-                accountOrContract.balance = (accountOrContract as VestingContractInfo)
-                    .calculateAvailableAmount(this.height, Nimiq.Policy.coinsToSatoshis(balance));
-            } else {
-                accountOrContract.balance = Nimiq.Policy.coinsToSatoshis(balance);
-            }
-        }
-
-        // Store updated wallets
-        for (const wallet of wallets) {
-            // Update IndexedDB
-            await WalletStore.Instance.put(wallet);
-
-            // Update Vuex
-            this.$addWallet(wallet);
-        }
-
-        // Cache height and balances
-        const cacheInput = {
-            timestamp: Date.now(),
-            height: this.height,
-            balances: Array.from(balances.entries()),
-        };
-        window.sessionStorage.setItem(Checkout.BALANCE_CHECK_STORAGE_KEY, JSON.stringify(cacheInput));
-
-        return balances;
-    }
-
-    private onHeadChange(head: Nimiq.BlockHeader | {height: number}) {
-        this.height = head.height;
-    }
-
-    private addConsensusListeners() {
-        const network = (this.$refs.network as Network);
-        network.$on(Network.Events.API_READY, () => this.status = 'Contacting seed nodes...');
-        network.$on(Network.Events.CONSENSUS_SYNCING, () => this.status = 'Syncing consensus...');
-        network.$on(Network.Events.CONSENSUS_ESTABLISHED, () => this.status = 'Requesting balances...');
-    }
-
-    private setAccountOrContract(walletId: string, address: string, isFromRequest = false) {
-        const nimiqAddress = Nimiq.Address.fromUserFriendlyAddress(address);
-        const senderAccount = this.wallets.find((wallet: WalletInfo) => wallet.id === walletId)!;
-        const senderContract = senderAccount.findContractByAddress(nimiqAddress);
-        const signer = senderAccount.findSignerForAddress(nimiqAddress)!;
-
-        // FIXME: Also handle active account we get from store
-        this.$setActiveAccount({
-            walletId: senderAccount.id,
-            userFriendlyAddress: (senderContract || signer).userFriendlyAddress,
-        });
-
-        // proceed to transaction signing
-        switch (senderAccount.type) {
-            case WalletType.LEDGER:
-                this.$rpc.routerPush(`${RequestType.SIGN_TRANSACTION}-ledger`);
-                return;
-            case WalletType.LEGACY:
-            case WalletType.BIP39:
-                if (!this.height) return;
-
-                // The next block is the earliest for which tx are accepted by standard miners
-                const validityStartHeight = this.height + 1
-                    - TX_VALIDITY_WINDOW
-                    + this.request.validityDuration;
-
-                const request: KeyguardClient.SignTransactionRequest = {
-                    layout: 'checkout',
-                    shopOrigin: this.rpcState.origin,
-                    appName: this.request.appName,
-                    shopLogoUrl: this.request.shopLogoUrl,
-
-                    keyId: senderAccount.keyId,
-                    keyPath: signer!.path,
-                    keyLabel: senderAccount.label,
-
-                    sender: (senderContract || signer).address.serialize(),
-                    senderType: senderContract ? senderContract.type : Nimiq.Account.Type.BASIC,
-                    senderLabel: (senderContract || signer).label,
-                    recipient: this.request.recipient.serialize(),
-                    recipientType: this.request.recipientType,
-                    // recipientLabel: '', // Checkout is using the shopOrigin instead
-                    value: this.request.value,
-                    fee: this.request.fee,
-                    validityStartHeight,
-                    data: this.request.data,
-                    flags: this.request.flags,
-
-                    fiatAmount: this.request.fiatAmount,
-                    fiatCurrency: this.request.fiatCurrency,
-                    time: this.request.time, // TODO convert times from server time to local time
-                    expires: this.request.paymentOptions.find((option) => option.currency === Currency.NIM)!.expires,
-                };
-
-                staticStore.keyguardRequest = request;
-                const client = this.$rpc.createKeyguardClient(isFromRequest);
-                client.signTransaction(request);
-                return;
-        }
-    }
-
-    private goToOnboarding(useReplace?: boolean) {
-        // Redirect to onboarding
-        staticStore.originalRouteName = RequestType.CHECKOUT;
-        if (useReplace) {
-            this.$rpc.routerReplace(RequestType.ONBOARD);
-        } else {
-            this.$rpc.routerPush(RequestType.ONBOARD);
-        }
     }
 
     private close() {
         this.$rpc.reject(new Error(ERROR_CANCELED));
     }
 
-    private get hasSufficientBalanceAccount(): boolean {
-        return this.wallets.some((wallet: WalletInfo) => [...wallet.accounts.values(), ...wallet.contracts]
-            .some((info: AccountInfo | ContractInfo) => !!info.balance && info.balance >= this.minBalance));
+    private updateSelection(selectedCurrency: string) {
+        this.selectedCurrency = selectedCurrency as Currency;
     }
 
-    private get minBalance() {
-        return this.request.value + this.request.fee;
+    private chooseCurrency(currency: Currency) {
+        console.log('choosen', currency);
+        this.choosenCurrency = currency;
     }
 
-    private getLastBalanceUpdateHeight(): {timestamp: number, height: number, balances: Map<string, number>} | null {
-        const rawCache = window.sessionStorage.getItem(Checkout.BALANCE_CHECK_STORAGE_KEY);
-        if (!rawCache) return null;
-
-        try {
-            const cache: {timestamp: number, height: number, balances: Array<[string, number]>} = JSON.parse(rawCache);
-
-            // Check if expired or doesn't have a height
-            if (cache.timestamp < Date.now() - 5 * 60 * 1000 || cache.height === 0) throw new Error();
-
-            return Object.assign(cache, {
-                balances: new Map(cache.balances),
-            });
-        } catch (e) {
-            window.sessionStorage.removeItem(Checkout.BALANCE_CHECK_STORAGE_KEY);
-            return null;
-        }
+    private data() {
+        return {
+            Currency,
+        };
     }
 }
 </script>
 
 <style scoped>
-    .small-page {
+    .container >>> .small-page {
         position: relative;
     }
 
-    .status-screen {
-        position: absolute;
-        left: 0;
-        top: 0;
-    }
-
-    .nq-h1 {
+    .container >>> .nq-h1 {
         margin-top: 3.5rem;
         margin-bottom: 1rem;
         line-height: 1;
         text-align: center;
     }
 
-    .non-sufficient-balance {
-        text-align: center;
-        margin-top: 0.5rem;
-        margin-bottom: 0.5rem;
-        flex-shrink: 0;
+    .carousel >>> .currency-info {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        transition: transform 0.5s cubic-bezier(.67,0,.16,1);
+        transform: translateY(0rem);
+    }
+    .carousel > :not(.selected) >>> .currency-info {
+        transform: translateY(-6rem);
     }
 
-    .non-sufficient-balance .nq-text {
-        opacity: 1;
-        font-weight: 600;
+    .carousel {
+        width: 100%;
+        box-sizing: border-box;
+        padding: 0;
+        overflow: hidden;
     }
 
-    .non-sufficient-balance .nq-button-s {
-        color: white;
-        line-height: 3.375rem;
+    .carousel >>> .payment-option {
+        transition: filter .5s ease;
+        padding: 4rem 0;
     }
 
-    .non-sufficient-balance .nq-icon {
-        font-size: 2.25rem;
-        vertical-align: text-bottom;
+    .carousel :not(.selected) >>> .payment-option {
+        -webkit-filter: grayscale(100%); /* Safari 6.0 - 9.0 */
+        filter: grayscale(100%);
     }
 </style>
 
