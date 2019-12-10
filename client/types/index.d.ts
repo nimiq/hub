a7202c3cd115977b798ffa92b8bf2fd0193b0f5b
Daniel <github.danimoh@neverbox.com>
2019-12-09 18:16:57 -0600

    client: make type exports work
    - Specify "types" in package.json
    - Add @nimiq/core-web as a depencecy such that core types are included
    - Add big-integer as a dependency to include types
    - Expose PopupRequestBehavior and CashlinkState enum
    - Export PublicRequestTypes and RequestBehavior types for convenience

--- client/types/index.d.ts
+++ client/types/index.d.ts
@@ -1,1 +1,9 @@
-export * from '../dist/src/client/HubApi';
+// Import @nimiq/core-web types from relative path within user's node_modules
+/// <reference path="../../core-web/namespace.d.ts" />
+
+export { default } from '../dist/src/client/HubApi';
+
+// export public request types and RequestBehavior types for convenience of the HupApi user
+export * from '../dist/src/src/lib/PublicRequestTypes';
+export { RequestType } from '../dist/src/src/lib/RequestTypes';
+export { PopupRequestBehavior, RedirectRequestBehavior } from '../dist/src/client/RequestBehavior';
