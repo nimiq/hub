4db2cd8bb2c4852d771db063e6ce9db97fb28461
Daniel <github.danimoh@neverbox.com>
2020-03-03 16:58:21 +0100

    Checkout: show background card placeholders only on desktop

--- src/views/Checkout.vue
+++ src/views/Checkout.vue
@@ -349,147 +349,149 @@ export default Checkout;
         pointer-events: all !important;
     }
 
-    /* Show placeholders when card is not selected */
-    .carousel >>> .payment-option:not(.confirmed) .timer,
-    .carousel >>> .payment-option:not(.confirmed) .nq-button,
-    .carousel >>> .payment-option:not(.confirmed) .nq-button-s,
-    .carousel >>> .payment-option:not(.confirmed) .nq-button-pill,
-    .carousel >>> .payment-option:not(.confirmed) .nq-card > .nq-h1,
-    .carousel >>> .payment-option:not(.confirmed) .info-line .account,
-    .carousel >>> .payment-option:not(.confirmed) .info-line .amounts,
-    .carousel >>> .payment-option:not(.confirmed) .nq-card-body .label,
-    .carousel >>> .payment-option:not(.confirmed) .nq-card-body .amounts .crypto,
-    .carousel >>> .payment-option:not(.confirmed) .nq-card-body .amounts .fee,
-    .carousel >>> .payment-option:not(.confirmed) .account-list .amount,
-    .carousel >>> .payment-option:not(.confirmed) .nq-card-footer .nq-link,
-    .carousel >>> .payment-option:not(.confirmed) .account-selector .wallet-label,
-    .carousel >>> .payment-option:not(.confirmed) .nq-card-body .identicon-and-label,
-    .carousel >>> .payment-option:not(.confirmed) .account-list .identicon-and-label > *,
-    .carousel >>> .payment-option:not(.confirmed) .nq-card .non-sufficient-balance .nq-text {
-        position: relative;
-    }
+    /* On desktop show placeholders when card is not selected */
+    @media (min-width: 451px) {
+        .carousel >>> .payment-option:not(.confirmed) .timer,
+        .carousel >>> .payment-option:not(.confirmed) .nq-button,
+        .carousel >>> .payment-option:not(.confirmed) .nq-button-s,
+        .carousel >>> .payment-option:not(.confirmed) .nq-button-pill,
+        .carousel >>> .payment-option:not(.confirmed) .nq-card > .nq-h1,
+        .carousel >>> .payment-option:not(.confirmed) .info-line .account,
+        .carousel >>> .payment-option:not(.confirmed) .info-line .amounts,
+        .carousel >>> .payment-option:not(.confirmed) .nq-card-body .label,
+        .carousel >>> .payment-option:not(.confirmed) .nq-card-body .amounts .crypto,
+        .carousel >>> .payment-option:not(.confirmed) .nq-card-body .amounts .fee,
+        .carousel >>> .payment-option:not(.confirmed) .account-list .amount,
+        .carousel >>> .payment-option:not(.confirmed) .nq-card-footer .nq-link,
+        .carousel >>> .payment-option:not(.confirmed) .account-selector .wallet-label,
+        .carousel >>> .payment-option:not(.confirmed) .nq-card-body .identicon-and-label,
+        .carousel >>> .payment-option:not(.confirmed) .account-list .identicon-and-label > *,
+        .carousel >>> .payment-option:not(.confirmed) .nq-card .non-sufficient-balance .nq-text {
+            position: relative;
+        }
 
-    .carousel >>> .payment-option:not(.confirmed) .nq-button::after,
-    .carousel >>> .payment-option:not(.confirmed) .nq-button-s::after,
-    .carousel >>> .payment-option:not(.confirmed) .nq-button-pill::after {
-        transition: all .5s var(--nimiq-ease);
-    }
+        .carousel >>> .payment-option:not(.confirmed) .nq-button::after,
+        .carousel >>> .payment-option:not(.confirmed) .nq-button-s::after,
+        .carousel >>> .payment-option:not(.confirmed) .nq-button-pill::after {
+            transition: all .5s var(--nimiq-ease);
+        }
 
-    .carousel >>> .payment-option:not(.confirmed) .timer::after,
-    .carousel >>> > :not(.selected) .payment-option:not(.confirmed) .nq-button::after,
-    .carousel >>> > :not(.selected) .payment-option:not(.confirmed) .nq-button-s::after,
-    .carousel >>> > :not(.selected) .payment-option:not(.confirmed) .nq-button-pill::after,
-    .carousel >>> .payment-option:not(.confirmed) .nq-card > .nq-h1::after,
-    .carousel >>> .payment-option:not(.confirmed) .info-line .account::after,
-    .carousel >>> .payment-option:not(.confirmed) .info-line .amounts::after,
-    .carousel >>> .payment-option:not(.confirmed) .nq-card-body .label::after,
-    .carousel >>> .payment-option:not(.confirmed) .nq-card-body .amounts .crypto::after,
-    .carousel >>> .payment-option:not(.confirmed) .nq-card-body .amounts .fee::after,
-    .carousel >>> .payment-option:not(.confirmed) .account-list .amount::after,
-    .carousel >>> .payment-option:not(.confirmed) .nq-card-footer .nq-link::after,
-    .carousel >>> .payment-option:not(.confirmed) .account-selector .wallet-label::before,
-    .carousel >>> .payment-option:not(.confirmed) .nq-card-body .identicon-and-label::after,
-    .carousel >>> .payment-option:not(.confirmed) .account-list .identicon-and-label > *::after,
-    .carousel >>> .payment-option:not(.confirmed) .nq-card .non-sufficient-balance .nq-text::after {
-        --placeholder-size: 100%;
-        --placeholder-width: var(--placeholder-size);
-        --placeholder-height: var(--placeholder-size);
-        content: '';
-        position: absolute;
-        top: calc((100% - var(--placeholder-height)) / 2);
-        left: calc((100% - var(--placeholder-width)) / 2);
-        width: var(--placeholder-width);
-        height: var(--placeholder-height);
-        background-color: #f2f2f4; /* --nimiq-blue 0.06 opacity */
-        opacity: 0;
-        border: none;
-        border-radius: 500px;
-        z-index: 2;
-        transition: all .5s var(--nimiq-ease);
-    }
+        .carousel >>> .payment-option:not(.confirmed) .timer::after,
+        .carousel >>> > :not(.selected) .payment-option:not(.confirmed) .nq-button::after,
+        .carousel >>> > :not(.selected) .payment-option:not(.confirmed) .nq-button-s::after,
+        .carousel >>> > :not(.selected) .payment-option:not(.confirmed) .nq-button-pill::after,
+        .carousel >>> .payment-option:not(.confirmed) .nq-card > .nq-h1::after,
+        .carousel >>> .payment-option:not(.confirmed) .info-line .account::after,
+        .carousel >>> .payment-option:not(.confirmed) .info-line .amounts::after,
+        .carousel >>> .payment-option:not(.confirmed) .nq-card-body .label::after,
+        .carousel >>> .payment-option:not(.confirmed) .nq-card-body .amounts .crypto::after,
+        .carousel >>> .payment-option:not(.confirmed) .nq-card-body .amounts .fee::after,
+        .carousel >>> .payment-option:not(.confirmed) .account-list .amount::after,
+        .carousel >>> .payment-option:not(.confirmed) .nq-card-footer .nq-link::after,
+        .carousel >>> .payment-option:not(.confirmed) .account-selector .wallet-label::before,
+        .carousel >>> .payment-option:not(.confirmed) .nq-card-body .identicon-and-label::after,
+        .carousel >>> .payment-option:not(.confirmed) .account-list .identicon-and-label > *::after,
+        .carousel >>> .payment-option:not(.confirmed) .nq-card .non-sufficient-balance .nq-text::after {
+            --placeholder-size: 100%;
+            --placeholder-width: var(--placeholder-size);
+            --placeholder-height: var(--placeholder-size);
+            content: '';
+            position: absolute;
+            top: calc((100% - var(--placeholder-height)) / 2);
+            left: calc((100% - var(--placeholder-width)) / 2);
+            width: var(--placeholder-width);
+            height: var(--placeholder-height);
+            background-color: #f2f2f4; /* --nimiq-blue 0.06 opacity */
+            opacity: 0;
+            border: none;
+            border-radius: 500px;
+            z-index: 2;
+            transition: all .5s var(--nimiq-ease);
+        }
 
-    .carousel >>> .payment-option:not(.confirmed) .nq-card > .nq-h1::after {
-        --placeholder-width: 85%;
-    }
+        .carousel >>> .payment-option:not(.confirmed) .nq-card > .nq-h1::after {
+            --placeholder-width: 85%;
+        }
 
-    .carousel >>> .payment-option:not(.confirmed) .info-line .amounts::after,
-    .carousel >>> .payment-option:not(.confirmed) .info-line .account::after,
-    .carousel >>> .payment-option:not(.confirmed) .account-selector .wallet-label::before {
-        --placeholder-height: 3.25rem;
-        top: initial;
-        box-shadow: 0 0 0 1rem var(--nimiq-card-bg);
-    }
+        .carousel >>> .payment-option:not(.confirmed) .info-line .amounts::after,
+        .carousel >>> .payment-option:not(.confirmed) .info-line .account::after,
+        .carousel >>> .payment-option:not(.confirmed) .account-selector .wallet-label::before {
+            --placeholder-height: 3.25rem;
+            top: initial;
+            box-shadow: 0 0 0 1rem var(--nimiq-card-bg);
+        }
 
-    .carousel >>> .payment-option:not(.confirmed) .timer::after,
-    .carousel >>> > :not(.selected) .payment-option:not(.confirmed) .nq-button::after,
-    .carousel >>> > :not(.selected) .payment-option:not(.confirmed) .nq-button-s::after,
-    .carousel >>> > :not(.selected) .payment-option:not(.confirmed) .nq-button-pill::after {
-        --placeholder-size: 105%;
-    }
+        .carousel >>> .payment-option:not(.confirmed) .timer::after,
+        .carousel >>> > :not(.selected) .payment-option:not(.confirmed) .nq-button::after,
+        .carousel >>> > :not(.selected) .payment-option:not(.confirmed) .nq-button-s::after,
+        .carousel >>> > :not(.selected) .payment-option:not(.confirmed) .nq-button-pill::after {
+            --placeholder-size: 105%;
+        }
 
-    .carousel >>> .payment-option:not(.confirmed) .nq-card-body .identicon-and-label::after {
-        --placeholder-size: 21rem;
-        top: initial;
-        left: initial;
-        box-shadow: 0 0 0 4rem var(--nimiq-card-bg);
-    }
+        .carousel >>> .payment-option:not(.confirmed) .nq-card-body .identicon-and-label::after {
+            --placeholder-size: 21rem;
+            top: initial;
+            left: initial;
+            box-shadow: 0 0 0 4rem var(--nimiq-card-bg);
+        }
 
-    .carousel >>> .payment-option:not(.confirmed) .nq-card .non-sufficient-balance .nq-text::after {
-        --placeholder-width: 90%;
-    }
+        .carousel >>> .payment-option:not(.confirmed) .nq-card .non-sufficient-balance .nq-text::after {
+            --placeholder-width: 90%;
+        }
 
-    .carousel >>> .payment-option:not(.confirmed) .nq-card > .nq-h1::after,
-    .carousel >>> .payment-option:not(.confirmed) .nq-card-body .label::after,
-    .carousel >>> .payment-option:not(.confirmed) .nq-card-body .amounts .crypto::after,
-    .carousel >>> .payment-option:not(.confirmed) .nq-card-footer .nq-link::after,
-    .carousel >>> .payment-option:not(.confirmed) .nq-card .non-sufficient-balance .nq-text::after {
-        box-shadow: 0 0 0 .6rem var(--nimiq-card-bg);
-    }
+        .carousel >>> .payment-option:not(.confirmed) .nq-card > .nq-h1::after,
+        .carousel >>> .payment-option:not(.confirmed) .nq-card-body .label::after,
+        .carousel >>> .payment-option:not(.confirmed) .nq-card-body .amounts .crypto::after,
+        .carousel >>> .payment-option:not(.confirmed) .nq-card-footer .nq-link::after,
+        .carousel >>> .payment-option:not(.confirmed) .nq-card .non-sufficient-balance .nq-text::after {
+            box-shadow: 0 0 0 .6rem var(--nimiq-card-bg);
+        }
 
-    .carousel >>> > :not(.selected) .payment-option:not(.confirmed) .timer::after,
-    .carousel >>> > :not(.selected) .payment-option:not(.confirmed) .nq-button::after,
-    .carousel >>> > :not(.selected) .payment-option:not(.confirmed) .nq-button-s::after,
-    .carousel >>> > :not(.selected) .payment-option:not(.confirmed) .nq-button-pill::after,
-    .carousel >>> > :not(.selected) .payment-option:not(.confirmed) .nq-card > .nq-h1::after,
-    .carousel >>> > :not(.selected) .payment-option:not(.confirmed) .info-line .account::after,
-    .carousel >>> > :not(.selected) .payment-option:not(.confirmed) .info-line .amounts::after,
-    .carousel >>> > :not(.selected) .payment-option:not(.confirmed) .nq-card-body .label::after,
-    .carousel >>> > :not(.selected) .payment-option:not(.confirmed) .account-list .amount::after,
-    .carousel >>> > :not(.selected) .payment-option:not(.confirmed) .nq-card-body .amounts .crypto::after,
-    .carousel >>> > :not(.selected) .payment-option:not(.confirmed) .nq-card-body .amounts .fee::after,
-    .carousel >>> > :not(.selected) .payment-option:not(.confirmed) .nq-card-footer .nq-link::after,
-    .carousel >>> > :not(.selected) .payment-option:not(.confirmed) .account-selector .wallet-label::before,
-    .carousel >>> > :not(.selected) .payment-option:not(.confirmed) .nq-card-body .identicon-and-label::after,
-    .carousel >>> > :not(.selected) .payment-option:not(.confirmed) .account-list .identicon-and-label > *::after,
-    .carousel >>> > :not(.selected) .payment-option:not(.confirmed) .nq-card .non-sufficient-balance .nq-text::after {
-        opacity: 1;
-    }
+        .carousel >>> > :not(.selected) .payment-option:not(.confirmed) .timer::after,
+        .carousel >>> > :not(.selected) .payment-option:not(.confirmed) .nq-button::after,
+        .carousel >>> > :not(.selected) .payment-option:not(.confirmed) .nq-button-s::after,
+        .carousel >>> > :not(.selected) .payment-option:not(.confirmed) .nq-button-pill::after,
+        .carousel >>> > :not(.selected) .payment-option:not(.confirmed) .nq-card > .nq-h1::after,
+        .carousel >>> > :not(.selected) .payment-option:not(.confirmed) .info-line .account::after,
+        .carousel >>> > :not(.selected) .payment-option:not(.confirmed) .info-line .amounts::after,
+        .carousel >>> > :not(.selected) .payment-option:not(.confirmed) .nq-card-body .label::after,
+        .carousel >>> > :not(.selected) .payment-option:not(.confirmed) .account-list .amount::after,
+        .carousel >>> > :not(.selected) .payment-option:not(.confirmed) .nq-card-body .amounts .crypto::after,
+        .carousel >>> > :not(.selected) .payment-option:not(.confirmed) .nq-card-body .amounts .fee::after,
+        .carousel >>> > :not(.selected) .payment-option:not(.confirmed) .nq-card-footer .nq-link::after,
+        .carousel >>> > :not(.selected) .payment-option:not(.confirmed) .account-selector .wallet-label::before,
+        .carousel >>> > :not(.selected) .payment-option:not(.confirmed) .nq-card-body .identicon-and-label::after,
+        .carousel >>> > :not(.selected) .payment-option:not(.confirmed) .account-list .identicon-and-label > *::after,
+        .carousel >>> > :not(.selected) .payment-option:not(.confirmed) .nq-card .non-sufficient-balance .nq-text::after {
+            opacity: 1;
+        }
 
-    .carousel >>> > :not(.selected) .payment-option:not(.confirmed) .amounts {
-        transition: border-top-color .5s var(--nimiq-ease);
-        border-top-color: var(--nimiq-card-bg);
-    }
+        .carousel >>> > :not(.selected) .payment-option:not(.confirmed) .amounts {
+            transition: border-top-color .5s var(--nimiq-ease);
+            border-top-color: var(--nimiq-card-bg);
+        }
 
-    .carousel >>> > :not(.selected) .payment-option:not(.confirmed) .nq-button {
-        transition: box-shadow .5s var(--nimiq-ease);
-        box-shadow: none;
-    }
+        .carousel >>> > :not(.selected) .payment-option:not(.confirmed) .nq-button {
+            transition: box-shadow .5s var(--nimiq-ease);
+            box-shadow: none;
+        }
 
-    .carousel >>> > :not(.selected) .payment-option:not(.confirmed) .arrow-runway {
-        transition: opacity .5s var(--nimiq-ease);
-        opacity: 0;
-    }
+        .carousel >>> > :not(.selected) .payment-option:not(.confirmed) .arrow-runway {
+            transition: opacity .5s var(--nimiq-ease);
+            opacity: 0;
+        }
 
-    .carousel >>> > :not(.selected) .payment-option:not(.confirmed) .arrow-runway * {
-        animation: unset; /* disable animation in background to avoid unnecessary rendering layers */
-    }
+        .carousel >>> > :not(.selected) .payment-option:not(.confirmed) .arrow-runway * {
+            animation: unset; /* disable animation in background to avoid unnecessary rendering layers */
+        }
 
-    .carousel >>> .payment-option:not(.confirmed) video {
-        transition: opacity .5s var(--nimiq-ease);
-    }
+        .carousel >>> .payment-option:not(.confirmed) video {
+            transition: opacity .5s var(--nimiq-ease);
+        }
 
-    .carousel >>> > :not(.selected) .payment-option:not(.confirmed) video {
-        opacity: 0;
+        .carousel >>> > :not(.selected) .payment-option:not(.confirmed) video {
+            opacity: 0;
+        }
     }
 
     .global-close {
