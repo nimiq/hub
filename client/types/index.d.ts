ca0bf609c38d28dfe29f89e1e59df716c26845a5
sisou <hello@soerenschwert.de>
2020-08-10 10:37:55 +0200

    Fix types as dependency

--- client/types/index.d.ts
+++ client/types/index.d.ts
@@ -1,8 +1,8 @@
 // Import @nimiq/core-web types from relative path within user's node_modules
 /// <reference path="../../core-web/namespace.d.ts" />
 
-export { default } from '../dist/src/client/HubApi';
+export { default } from '../dist/src/HubApi';
 
 // export public request types and RequestBehavior types for convenience of the HupApi user
-export * from '../dist/src/src/lib/PublicRequestTypes';
-export { PopupRequestBehavior, RedirectRequestBehavior } from '../dist/src/client/RequestBehavior';
+export * from '../dist/src/PublicRequestTypes';
+export { PopupRequestBehavior, RedirectRequestBehavior } from '../dist/src/RequestBehavior';
