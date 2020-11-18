ebf0dd2e6eff3b468e1be61805c1fb93d4bf4ae1
sisou <hello@soerenschwert.de>
2020-11-18 17:13:30 +0100

    Add Fastspot mainnet config

--- src/config/config.mainnet.ts
+++ src/config/config.mainnet.ts
@@ -23,7 +23,7 @@ export default {
     bitcoinAddressType: NATIVE_SEGWIT,
 
     fastspot: {
-        apiEndpoint: 'https://api.fastspot.io/fast/v1',
-        apiKey: '',
+        apiEndpoint: 'https://api.go.fastspot.io/fast/v1',
+        apiKey: 'c20d43d0-8f60-4fca-a298-85e80f64d042',
     },
 };
