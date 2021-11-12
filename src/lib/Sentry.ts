08c1174a68cceb0a645737c9a027c096ec8afdc6
Sören <hello@soerenschwert.de>
2021-11-12 11:44:30 -0600

    Update Sentry integration

--- src/lib/Sentry.ts
+++ src/lib/Sentry.ts
@@ -1,13 +1,13 @@
-import { init as initSentry, captureException } from '@sentry/browser';
-import { Vue as SentryVueIntegration } from '@sentry/integrations';
+import { init as initSentry, captureException } from '@sentry/vue';
 import Config from 'config';
 
 export function startSentry(Vue: any) {
     if (Config.reportToSentry) {
         initSentry({
-            dsn: 'https://92f2289fc2ac4c809dfa685911f865c2@sentry.io/1330855',
-            integrations: [new SentryVueIntegration({Vue, attachProps: true})],
+            dsn: 'https://92f2289fc2ac4c809dfa685911f865c2@o208918.ingest.sentry.io/1330855',
+            Vue,
             environment: Config.network,
+            attachProps: true,
         });
         Vue.prototype.$captureException = captureException;
     }
