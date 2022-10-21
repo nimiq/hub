8368307793b2fe5682cd8554441201ce4ca36d0a
Sören <hello@soerenschwert.de>
2022-10-21 11:02:05 +0200

    Narrow encryptionKey algorithm type

--- client/PublicRequestTypes.ts
+++ client/PublicRequestTypes.ts
@@ -634,7 +634,7 @@ export interface ConnectedAccount {
     encryptionKey: {
         format: 'spki',
         keyData: Uint8Array,
-        algorithm: RsaHashedImportParams,
+        algorithm: { name: string, hash: string },
         keyUsages: ['encrypt'],
     };
     account: {
