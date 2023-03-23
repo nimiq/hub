986734b7da455e6c5d0a14a8b79e45899dd5d6c1
Sören <hello@soerenschwert.de>
2023-03-22 22:03:40 -0600

    Add support for signing unstaking transactions

--- src/lib/Constants.ts
+++ src/lib/Constants.ts
@@ -47,6 +47,8 @@ export const HISTORY_KEY_SELECTED_CURRENCY = 'selected-currency';
 export const FIAT_API_PROVIDER = FiatApiProvider.CoinGecko;
 
 export const StakingTransactionType = {
+    UNSTAKE: 1,
+
     CREATE_STAKER: 5,
     STAKE: 6,
     UPDATE_STAKER: 7,
