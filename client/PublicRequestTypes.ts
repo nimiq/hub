068cfe77ccdc4e15fec773f5b44b06f31ee6e591
Sören <hello@soerenschwert.de>
2023-05-22 12:01:20 +0200

    Add SignMultisigTransactionRequest public request type

--- client/PublicRequestTypes.ts
+++ client/PublicRequestTypes.ts
@@ -16,6 +16,7 @@ export enum RequestType {
     CHECKOUT = 'checkout',
     SIGN_MESSAGE = 'sign-message',
     SIGN_TRANSACTION = 'sign-transaction',
+    SIGN_MULTISIG_TRANSACTION = 'sign-multisig-transaction',
     ONBOARD = 'onboard',
     SIGNUP = 'signup',
     LOGIN = 'login',
@@ -241,6 +242,37 @@ export interface SignedTransaction {
     };
 }
 
+export interface MultisigInfo {
+    publicKeys: Bytes[];
+    numberOfSigners: number;
+    signerPublicKeys?: Bytes[]; // Can be omitted when all publicKeys need to sign
+    secret: {
+        aggregatedSecret: Bytes;
+    } | {
+        encryptedSecrets: Bytes[];
+        bScalar: Bytes;
+    };
+    aggregatedCommitment: Bytes;
+    userName?: string;
+}
+
+export interface SignMultisigTransactionRequest extends BasicRequest {
+    signer: string; // Address
+
+    sender: string;
+    senderLabel: string;
+    recipient: string;
+    recipientType?: Nimiq.Account.Type;
+    recipientLabel?: string;
+    value: number;
+    fee?: number;
+    extraData?: Bytes;
+    flags?: number;
+    validityStartHeight: number; // FIXME To be made optional when hub has its own network
+
+    multisigConfig: MultisigInfo;
+}
+
 export interface NimiqHtlcCreationInstructions {
     type: 'NIM';
     sender: string; // My address, must be redeem address of HTLC, or if contract, its owner must be redeem address
@@ -446,6 +478,8 @@ export interface SignedMessage {
     signature: Uint8Array;
 }
 
+export type PartialSignature = SignedMessage;
+
 export interface Address {
     address: string; // Userfriendly address
     label: string;
@@ -625,6 +659,7 @@ export interface SignedPolygonTransaction {
 }
 
 export type RpcRequest = SignTransactionRequest
+                       | SignMultisigTransactionRequest
                        | CreateCashlinkRequest
                        | ManageCashlinkRequest
                        | CheckoutRequest
@@ -642,6 +677,7 @@ export type RpcRequest = SignTransactionRequest
                        | RefundSwapRequest;
 
 export type RpcResult = SignedTransaction
+                      | PartialSignature
                       | Account
                       | Account[]
                       | SimpleResult
