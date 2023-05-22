6f9b5a040146232b4bb63d74c81a6bf5b39fcf91
Sören <hello@soerenschwert.de>
2023-05-22 12:01:58 +0200

    Add encryption key params as required property to encrypted secrets

--- src/lib/RequestParser.ts
+++ src/lib/RequestParser.ts
@@ -820,6 +820,7 @@ export class RequestParser {
                                     (bytes) => parseBytes(bytes),
                                 ),
                                 bScalar: parseBytes(signMultisigTxRequest.multisigConfig.secret.bScalar),
+                                keyParams: signMultisigTxRequest.multisigConfig.secret.keyParams,
                             },
                         aggregatedCommitment: parseBytes(signMultisigTxRequest.multisigConfig.aggregatedCommitment),
                         userName: signMultisigTxRequest.multisigConfig.userName,
