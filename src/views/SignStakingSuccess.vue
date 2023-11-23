8a993826da66931f58a6f756f895e26461bab5ca
Sören <hello@soerenschwert.de>
2023-11-23 11:30:49 -0600

    Avoid using 1.0 Nimiq lib for transaction handling

--- src/views/SignStakingSuccess.vue
+++ src/views/SignStakingSuccess.vue
@@ -1,35 +1,43 @@
-<template>
-    <Network ref="network" :visible="false"/>
-</template>
-
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator';
-import Network from '@/components/Network.vue';
 import { SignedTransaction } from '../../client/PublicRequestTypes';
 import { State } from 'vuex-class';
-import { Static } from '../lib/StaticStore';
 import KeyguardClient from '@nimiq/keyguard-client';
-import { StakingSignallingTypes, StakingTransactionType } from '../lib/Constants';
+import { bytesToHex, hexToBytes } from '../lib/BufferUtils';
 
-@Component({components: {Network}})
+@Component({})
 export default class SignStakingSuccess extends Vue {
-    @Static private keyguardRequest!: KeyguardClient.SignStakingRequest;
     @State private keyguardResult!: KeyguardClient.SignStakingResult;
 
     private async mounted() {
-        const tx = await (this.$refs.network as Network).createTx(Object.assign(
-            { signerPubKey: this.keyguardResult.publicKey },
-            this.keyguardRequest,
-            this.keyguardResult,
-            StakingSignallingTypes.includes(this.keyguardRequest.type) ? { value: 0, flags: 0b10 } : {},
-            this.keyguardRequest.type === StakingTransactionType.UNSTAKE
-                ? { proofPrefix: new Uint8Array([StakingTransactionType.UNSTAKE])}
-                : {},
-        ));
-        const result: SignedTransaction = await (this.$refs.network as Network).makeSignTransactionResult(tx);
+        const { Transaction } = await window.loadAlbatross();
 
-        // Overwrite serializedTx with Albatross serialization format
-        result.serializedTx = Nimiq.BufferUtils.toHex(this.keyguardResult.serializedTx);
+        const hex = bytesToHex(this.keyguardResult.serializedTx);
+        const tx = Transaction.fromAny(hex);
+        const plain = tx.toPlain();
+
+        const result: SignedTransaction = {
+            serializedTx: hex,
+            hash: plain.transactionHash,
+            raw: {
+                ...plain,
+                senderType: tx.senderType,
+                recipientType: tx.recipientType,
+                proof: tx.proof,
+                signerPublicKey: 'publicKey' in plain.proof
+                    ? hexToBytes(plain.proof.publicKey)
+                    : 'creatorPublicKey' in plain.proof
+                        ? hexToBytes(plain.proof.creatorPublicKey)
+                        : new Uint8Array(0),
+                signature: 'signature' in plain.proof
+                    ? hexToBytes(plain.proof.signature)
+                    : 'creatorSignature' in plain.proof
+                        ? hexToBytes(plain.proof.creatorSignature)
+                        : new Uint8Array(0),
+                extraData: tx.data,
+                networkId: tx.networkId,
+            },
+        };
 
         this.$rpc.resolve(result);
     }
