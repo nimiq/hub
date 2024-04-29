71f61abcf77c4246ed4cb354d483b59e7e13493d
Sören <hello@soerenschwert.de>
2024-04-29 14:02:17 +0200

    Update Albatross client to one fixed for maintaining connections
    Add another seed node that is still up.

--- src/config/config.testnet.ts
+++ src/config/config.testnet.ts
@@ -6,7 +6,10 @@ export default {
     keyguardEndpoint: process.env.VUE_APP_KEYGUARD_URL,
     network: NETWORK_TEST,
     networkEndpoint: 'https://network.nimiq-testnet.com',
-    seedNodes: [process.env.VUE_APP_SEED_NODE_MULTIADDR],
+    seedNodes: [
+        process.env.VUE_APP_SEED_NODE_MULTIADDR,
+        '/dns4/faucet.pos.nimiq-testnet.com/tcp/8443/wss',
+    ],
     privilegedOrigins: [
         process.env.VUE_APP_WALLET_URL,
         process.env.VUE_APP_HUB_URL, // For testing with the deployed demos.html page
