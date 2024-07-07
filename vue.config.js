672cb975c1f6c35537d70bb0fd06d1f69091d51b
Daniel <github.danimoh@neverbox.com>
2024-07-07 11:24:26 +0200

    Add and configure babel to be able to use latest js syntax in code and deps
    - babel was included in the initial project setup via @vue/cli-plugin-babel, but removed
      already in 2018 in commit 37e9e693 "add peer dependencies", more likely by accident.
      After that and until now, the devDependencies in package.json still included babel-core,
      @babel/core and @vue/babel-preset-app, which though, from what it seems, were only dead
      dependencies, not involved in the build process: the generated webpack config (as can
      be checked via "env build=mainnet yarn vue-cli-service inspect --mode production") did
      not include babel-loader (which @vue/cli-plugin-babel and @vue/cli-plugin-typescript
      inject if @vue/cli-plugin-babel is present), and removing the dead dependencies did not
      result in a changed build result apart from changed file hashes, based on comparing
      several built files.
    - Now @vue/cli-plugin-babel has been properly added to the project again to transpile code
      and dependencies with js syntax too modern for older browsers and our old webpack version
      via the babel-loader webpack loader. This is in line with the setup for our other Webpack
      based vue projects like the Wallet or vue-components, where babel is also enabled. This
      now allows to use more modern language features.
    - The previously dead dependencies babel-core, @babel/core and @vue/babel-preset-app
      have been removed as direct dependencies. They are now not dead anymore, but already
      included as transitive dependencies of @vue/cli-plugin-babel.
    - Dependencies that should be transpiled, must be listed in transpileDependencies in
      vue.config.js, which makes @vue/cli-plugin-babel process the listed dependencies with the
      babel-loader webpack loader.
    - To support the latest js syntax, @vue/cli-plugin-babel has been added at the latest
      available 4.x version compatible with webpack 4. This uses the latest @babel/preset-env.
    - Also update core-js for the latest polyfills to be available to babel.
    - Finally, also update @vue/cli-plugin-typescript, @vue/cli-plugin-unit-jest,
      @vue/cli-plugin-vuex and @vue/cli-service to be in line with @vue/cli-plugin-babel.

--- vue.config.js
+++ vue.config.js
@@ -249,6 +249,11 @@ module.exports = {
             }]);
     },
 
+    // Add dependencies here which should be transpiled by babel-loader via @vue/cli-plugin-babel. This is needed as our
+    // Webpack version is too old to process some modern js syntax in the listed dependencies.
+    // When changing to Webpack 5, some or all can probably be removed.
+    transpileDependencies: [],
+
     // For iOS debugging in BrowserStack, BrowserStack's localhost tunnel bs-local.com needs to be used, see
     // https://www.browserstack.com/docs/live/local-testing/ios-troubleshooting-guide. However, on bs-local.com features
     // only available on https like crypto.subtle or service workers do not work unless served as https. The Wallet also
