ccde1862c53869111add2c5a9fd170aaeb042d3d
Daniel <github.danimoh@neverbox.com>
2026-09-22 19:34:01 +0200

    vue.config.js: support dev server serving with a custom https certificate and key

--- vue.config.js
+++ vue.config.js
@@ -232,16 +232,22 @@ module.exports = {
     // When changing to Webpack 5, some or all can probably be removed.
     transpileDependencies: ['@nimiq/utils', '@nimiq/ledger-api'],
 
-    // For iOS debugging in BrowserStack, BrowserStack's localhost tunnel bs-local.com needs to be used, see
-    // https://www.browserstack.com/docs/live/local-testing/ios-troubleshooting-guide. However, on bs-local.com features
-    // only available on https like crypto.subtle or service workers do not work unless served as https. The Wallet also
-    // needs to served with this option and the Keyguard has to be served over https as well. Safari has problems to
-    // open the https page with invalid certificate, but Chrome iOS works and also uses the Safari engine. To be able to
-    // use the dev tools with Chrome iOS on BrowserStack, launch BrowserStack with Safari first, then switch to Chrome.
-    ...(process.env['browserstack-ios-testing'] ? {
+    ...((process.env['https-key'] && process.env['https-cert'])
+        // For iOS debugging in BrowserStack, BrowserStack's localhost tunnel bs-local.com needs to be used, see
+        // https://www.browserstack.com/docs/live/local-testing/ios-troubleshooting-guide. However, on bs-local.com
+        // features only available on https like crypto.subtle or service workers don't work unless served as https.
+        // The Wallet also needs to be served with this option and the Keyguard has to be served over https as well.
+        // Safari has problems to open the https page with invalid certificate, but Chrome iOS works and also uses
+        // the Safari engine. To be able to use the dev tools with Chrome iOS on BrowserStack, launch BrowserStack
+        // with Safari first, then switch to Chrome.
+        || process.env['browserstack-ios-testing'] ? {
         devServer: {
             https: true,
-            disableHostCheck: true,
+            disableHostCheck: true, // allow to serve locally on wallet.nimiq.com via custom cert, or on bs-local.com
+            ...(process.env['https-key'] && process.env['https-cert'] ? {
+                key: fs.readFileSync(process.env['https-key']),
+                cert: fs.readFileSync(process.env['https-cert']),
+            } : null),
         },
     } : null),
 };
